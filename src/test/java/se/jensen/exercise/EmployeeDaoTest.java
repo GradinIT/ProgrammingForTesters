481654c961ed72abce4c1fb9dde739a85e9839f5
jocke <joakim.gradin@gradin-it.com>
2020-10-23 10:21:06 +0200

    adding test cases

--- src/test/java/se/jensen/exercise/EmployeeDaoTest.java
+++ src/test/java/se/jensen/exercise/EmployeeDaoTest.java
@@ -1,15 +1,21 @@
 package se.jensen.exercise;
 
 import com.google.common.collect.Maps;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import se.jensen.dao.EmployeeFakeDao;
+import se.jensen.dao.EntityAlreadyInStorageException;
+import se.jensen.dao.EntityNotFoundException;
 import se.jensen.entity.Employee;
 
+import java.math.BigDecimal;
 import java.util.Map;
+
+import static org.junit.Assert.fail;
 
 public class EmployeeDaoTest {
     private EmployeeFakeDao employeeDao;
-
     private Map<Integer, Employee> storage = Maps.newHashMap();
 
     @Before
@@ -18,4 +24,46 @@ public class EmployeeDaoTest {
         employeeDao.setTestData();
     }
 
+    @Test
+    public void testThatEntityNotFoundExceptionIsThrown() {
+        try {
+            employeeDao.getEmployee(10);
+            fail(); //TODO: if we dont get an exception then fail
+        }
+        catch (EntityNotFoundException e) {
+
+        }
+        catch (Exception e) {
+            fail(); //TODO: if we get any other exeption then fail
+        }
+    }
+
+    @Test(expected = EntityAlreadyInStorageException.class)
+    public void testThatEntityAlreadyInStorageExceptionIsThrown() {
+        Employee employeeAlreadyInStorage = storage.get(1);
+        employeeDao.create(employeeAlreadyInStorage);
+    }
+
+    @Test
+    public void testThatNewEmployeeIsStore(){
+        Integer employeeId = 10;
+        String firstName = "firstName";
+        String lastName = "lastName";
+        BigDecimal salary = BigDecimal.valueOf(10000);
+        Boolean fullTime = Boolean.TRUE;
+
+        Employee employee = Employee.builder()
+                .setEmployeeId(employeeId)
+                .setLastName(lastName)
+                .setFirstName(firstName)
+                .setFullTime(fullTime)
+                .setSalary(salary)
+                .build();
+
+        Employee employeeCreated = employeeDao.create(employee);
+        Employee employeeInStorage = storage.get(employeeId);
+        Assert.assertNotNull(employeeInStorage);
+        Assert.assertEquals(employeeCreated,employeeInStorage);
+
+    }
 }
