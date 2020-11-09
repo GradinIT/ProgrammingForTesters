45a4739a2f7b151b2a692c665f6f781a3fcdb31d
jocke <joakim.gradin@gradin-it.com>
2020-11-09 16:03:19 +0100

    added test for delete update and create using rest

--- src/test/java/se/jensen/RestApiManualTest.java
+++ src/test/java/se/jensen/RestApiManualTest.java
@@ -2,6 +2,7 @@ package se.jensen;
 
 import se.jensen.api.EmployeeModel;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class RestApiManualTest {
@@ -12,10 +13,43 @@ public class RestApiManualTest {
         }
 
         //TODO: using the RestClient add a new Employee
+        EmployeeModel employeeModel = EmployeeModel.builder()
+                .setEmployeeId(80)
+                .setFullTime(Boolean.TRUE)
+                .setSalary(BigDecimal.valueOf(25000))
+                .setLastName("Andersson")
+                .setFirstName("Mr")
+                .build();
 
+        employeeModel = RestServiceClient.createEmployee(employeeModel).get();
+        System.out.println("after create before update: "+employeeModel);
+
+        EmployeeModel updatedEmployee = EmployeeModel.builder()
+                .setEmployeeId(employeeModel.getEmployeeId())
+                .setLastName(employeeModel.getLastName())
+                .setSalary(employeeModel.getSalary())
+                .setFullTime(employeeModel.getFullTime())
+                .setFirstName("Anders")
+                .build();
         //TODO: using te RestClient update the Employee with new First name
+        updatedEmployee = RestServiceClient.updateEmployee(updatedEmployee).get();
+        System.out.println("after update "+updatedEmployee);
+
+        employeeModels = RestServiceClient.getAllEmployees().get();
+        for (EmployeeModel e : employeeModels) {
+            System.out.println(e);
+        }
 
         //TODO: using te Rest client remove the new Employee
+
+        EmployeeModel deletedEmpoyee = RestServiceClient.deleteEmployee(updatedEmployee).get();
+
+        System.out.println("was deleted "+deletedEmpoyee);
+
+        employeeModels = RestServiceClient.getAllEmployees().get();
+        for (EmployeeModel e : employeeModels) {
+            System.out.println(e);
+        }
 
     }
 }
