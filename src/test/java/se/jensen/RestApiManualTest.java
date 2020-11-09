e7d0eed2c4c53d8b98b65b032b0371d3bf78ad74
jocke <joakim.gradin@gradin-it.com>
2020-11-09 10:39:22 +0100

    adding test cases

--- src/test/java/se/jensen/RestApiManualTest.java
+++ src/test/java/se/jensen/RestApiManualTest.java
@@ -3,9 +3,12 @@ package se.jensen;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import se.jensen.api.EmployeeModel;
 import se.jensen.eazy.inheritence.Car;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RestApiManualTest {
     public static void main(String[] args) throws Exception {
@@ -15,6 +18,7 @@ public class RestApiManualTest {
 
 
         ResponseEntity<List> listResponse = restTemplate.exchange("http://localhost:8080/car", HttpMethod.GET,null,List.class);
-        System.out.println(response.getBody());
+        List list = (List) listResponse.getBody();
+        System.out.println(list);
     }
 }
