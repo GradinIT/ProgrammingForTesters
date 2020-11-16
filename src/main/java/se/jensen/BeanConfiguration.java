860c7d416727dd4477afee495ad08cee25f402c4
jocke <joakim.gradin@gradin-it.com>
2020-11-16 12:56:39 +0100

    adding test cases

--- src/main/java/se/jensen/BeanConfiguration.java
+++ src/main/java/se/jensen/BeanConfiguration.java
@@ -7,15 +7,13 @@ import se.jensen.dao.EmployeeDao;
 import se.jensen.service.EmployeeService;
 import se.jensen.service.EmployeeServiceImpl;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class BeanConfiguration {
     @Autowired
     EmployeeDao primaryDataSource;
+
     @Bean
     public EmployeeService employeeService() {
         return new EmployeeServiceImpl(primaryDataSource);
     }
-
 }
