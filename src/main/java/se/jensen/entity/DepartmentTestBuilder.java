61fb244a3b6e86bbc0b670979df9373aebd10f10
jocke <joakim.gradin@gradin-it.com>
2020-11-23 14:40:01 +0100

    adding github actions

--- src/main/java/se/jensen/entity/DepartmentTestBuilder.java
+++ src/main/java/se/jensen/entity/DepartmentTestBuilder.java
@@ -1,2 +1,8 @@
 package se.jensen.entity;public class DepartmentTestBuilder {
+    public Department build() {
+        return Department.builder()
+                .departmentId(1)
+                .departmentName("Development")
+                .build();
+    }
 }
