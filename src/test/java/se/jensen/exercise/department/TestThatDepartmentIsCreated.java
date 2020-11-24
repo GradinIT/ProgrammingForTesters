ab38edc29bfbda813245836437a6a609b646b275
Shaikh <kashifa.nibras@gmail.com>
2020-11-24 12:39:41 +0100

    commiting testdepartmentIsCreated file to local repo

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,4 +1,24 @@
 package se.jensen.exercise.department;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.jensen.entity.Department;
+import se.jensen.entity.DepartmentTestBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestThatDepartmentIsCreated {
+        Department department = Department.builder()
+                .departmentName("Finance")
+                .departmentId(2)
+                .build();
+
+        @Test
+        public void testDepartmentIsCreated (){
+            System.out.println("testing whether department finance is created");
+            Assert.assertNotNull(department);
+            assertEquals(2, department.getDepartmentId());
+            assertEquals("Finance", department.getDepartmentName());
+        }
 }
