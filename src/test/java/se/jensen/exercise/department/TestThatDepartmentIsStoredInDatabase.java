4e6866d729dccec8fdcc240514121203a7d926b3
yulia-gutorova <j.gutorova@gmail.com>
2020-11-24 20:50:41 +0100

    Delete my tests

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsStoredInDatabase.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsStoredInDatabase.java
@@ -1,46 +1,4 @@
 package se.jensen.exercise.department;
-import se.jensen.dao.*;
-
-import liquibase.pro.packaged.I;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.atLeastOnce;
 
 public class TestThatDepartmentIsStoredInDatabase {
-
-        DepartmentDao departmentDao = mock(DepartmentDao.class);   // Mock the DB
-        private final Integer DEPARTMENTID = 1;
-        private final String  DEPARTMENTNAME = "newDepartment";
-
-        @Before
-        public void setUpMock()
-        {
-            when(departmentDao.save(any(DepartmentDatabaseEntry.class)))
-                    .thenReturn(DepartmentDatabaseEntry.builder()
-                            .departmentId(DEPARTMENTID)
-                            .departmentName(DEPARTMENTNAME)
-                            .build());
-        }
-
-        @Test
-        public void testIsStored()
-        {
-            DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
-                    .departmentId(DEPARTMENTID)
-                    .departmentName(DEPARTMENTNAME)
-                    .build();
-            DepartmentDatabaseEntry departmentDatabaseEntrySaved = departmentDao.save(departmentDatabaseEntry);
-
-            Assert.assertEquals(departmentDatabaseEntry.getDepartmentId(),departmentDatabaseEntrySaved.getDepartmentId());
-            Assert.assertEquals(departmentDatabaseEntry.getDepartmentName(),departmentDatabaseEntrySaved.getDepartmentName());
-        }
-
 }
