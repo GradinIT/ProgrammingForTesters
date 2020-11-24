693cf6affdef16da868b134a1292155d9c7a7b35
yulia-gutorova <j.gutorova@gmail.com>
2020-11-24 16:32:42 +0100

    YuliasChanges

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,4 +1,30 @@
 package se.jensen.exercise.department;
 
+import org.junit.*;
+import org.junit.experimental.categories.Category;
+import se.jensen.entity.Department;
+
+import se.jensen.test.category.UnitTest;
+
+@Category(UnitTest.class)
 public class TestThatDepartmentIsCreated {
+
+    @Test
+    public void testThatDepartmentIsCreated()
+    {
+        System.out.println("Test that department is created correctly");
+        Integer DEPARTMENTID = 1;
+        String DEPARTMENTNAME= "newDepartment";
+
+        Department department = Department.builder()
+                .departmentId(DEPARTMENTID)
+                .departmentName(DEPARTMENTNAME)
+                .build();
+
+        Assert.assertNotNull(department);
+        Assert.assertEquals(DEPARTMENTID, department.getDepartmentId());
+        Assert.assertEquals(DEPARTMENTNAME, department.getDepartmentName());
+    }
+
 }
+
