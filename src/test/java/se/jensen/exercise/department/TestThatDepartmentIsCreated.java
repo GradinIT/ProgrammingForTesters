8ff0925a78a16f0d9cc03d3244d801b03382e06f
Simon  Hällström <simonhallstrom@outlook.com>
2020-11-24 18:50:45 +0100

    frist Commit of unit test

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,4 +1,24 @@
 package se.jensen.exercise.department;
 
+import org.junit.Assert;
+import org.junit.Test;
+import se.jensen.entity.Department;
+
+import java.math.BigDecimal;
+
 public class TestThatDepartmentIsCreated {
+
+    @Test
+    public void test() {
+        String departmentName = "departmentName";
+        Integer departmentId = 2;
+
+        Department department = Department.builder()
+                .departmentName(departmentName)
+                .departmentId(departmentId)
+                .build();
+
+        Assert.assertEquals(departmentId,department.getDepartmentId());
+        Assert.assertEquals(departmentName,department.getDepartmentName());
+    }
 }
