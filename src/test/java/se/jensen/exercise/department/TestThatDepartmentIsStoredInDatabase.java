3259591f634d7cec2a242f0a278fe651a4e9f75c
Subhashini <sarathy.na@gmail.com>
2020-11-25 21:48:43 +0100

    commit

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsStoredInDatabase.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsStoredInDatabase.java
@@ -1,4 +1,36 @@
 package se.jensen.exercise.department;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.jensen.dao.DepartmentDatabaseEntry;
+import se.jensen.dao.DepartmentDao;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
 public class TestThatDepartmentIsStoredInDatabase {
+    DepartmentDao departmentDao= mock(DepartmentDao.class);
+    private final Integer DEPARTMENTID = 1;
+    private final String DEPARTMENTNAME = "DepartmentName";
+
+    @Before
+    public void setUpMock() {
+        when(departmentDao.save(any(DepartmentDatabaseEntry.class)))
+                .thenReturn(DepartmentDatabaseEntry.builder()
+                        .departmentId(DEPARTMENTID)
+                        .departmentName(DEPARTMENTNAME)
+                        .build());
+    }
+    @Test
+    public void testIsStored(){
+        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
+                .departmentId(DEPARTMENTID)
+                .departmentName(DEPARTMENTNAME)
+                .build();
+        System.out.println("start test");
+        DepartmentDatabaseEntry departmentDatabaseEntrySaved = departmentDao.save(departmentDatabaseEntry);
+        Assert.assertEquals(departmentDatabaseEntry.getDepartmentId(),departmentDatabaseEntrySaved.getDepartmentId());
+        System.out.println("Test finish");
+        verify(departmentDao,times(1));
+    }
 }
--- src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
+++ src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
@@ -1,4 +1,26 @@
 package se.jensen.exercise.department;
 
+
+import org.junit.Test;
+import se.jensen.entity.Department;
+
 public class TestThatNullPointerExceptionIsThrown {
+
+    @Test(expected = NullPointerException.class)
+    public void testDepartmentIdIsNull(){
+         Department.builder()
+                .departmentId(null)
+                .departmentName("Finance")
+                .build();
+        //Assert.assertEquals(null,department.getDepartmentId());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testDepartmentNameIsNull(){
+        Department.builder()
+                .departmentId(1)
+                .departmentName(null)
+                .build();
+       // Assert.assertEquals(null,department.getDepartmentName());
+    }
 }
