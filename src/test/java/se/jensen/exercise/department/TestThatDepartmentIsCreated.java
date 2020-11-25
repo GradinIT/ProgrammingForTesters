0590bb74a1d8f611b26e92e1b36b8617611cd93d
Subhashini <sarathy.na@gmail.com>
2020-11-25 17:58:50 +0100

    Department created

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,4 +1,21 @@
 package se.jensen.exercise.department;
 
+import org.junit.Assert;
+import org.junit.Test;
+import se.jensen.entity.Department;
+
 public class TestThatDepartmentIsCreated {
+
+        @Test
+        public void test() {
+            Integer departmentId = 1;
+            String departmentName = "Accounts";
+            Department department = Department.builder()
+                    .departmentId(departmentId)
+                    .departmentName(departmentName)
+                    .build();
+            Assert.assertEquals(departmentId, department.getDepartmentId());
+            Assert.assertEquals(departmentName, department.getDepartmentName());
+        }
+
 }
