5cc931a0c8842e73b21f23c1b9863ca27b281879
Shaikh <kashifa.nibras@gmail.com>
2020-11-25 10:33:50 +0100

    wrote testnullpointer code

--- src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
+++ src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
@@ -1,4 +1,21 @@
 package se.jensen.exercise.department;
 
+import org.junit.Test;
+import se.jensen.entity.Department;
+
 public class TestThatNullPointerExceptionIsThrown {
+    @Test(expected = NullPointerException.class)
+    public void testDepartmentIsIdNull(){
+        Department.builder().departmentId(null)
+                .departmentName("Finance")
+                .build();
+
+    }
+    @Test(expected = NullPointerException.class)
+    public void testDepartmentNameIsIdNull(){
+        Department.builder().departmentId(1)
+                .departmentName(null)
+                .build();
+
+    }
 }
