5c57c33609e8a2d795cb2f8ab19549258900d301
MusseJ <73593259+MusseJ@users.noreply.github.com>
2020-11-27 12:38:32 +0100

    Wrote a test and checked for null

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,8 +1,12 @@
 package se.jensen.exercise.department;
 
 import org.junit.Assert;
+
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import se.jensen.entity.Department;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
 
 public class TestThatDepartmentIsCreated {
     @Test
@@ -18,5 +22,38 @@ public class TestThatDepartmentIsCreated {
 
         Assert.assertEquals(departmentId, department.getDepartmentId());
         Assert.assertEquals(departmentName, department.getDepartmentName());
+    }
+
+    @Test
+    @DisplayName("Checks if the department is created")
+    public void CheckIfTheDepartmentIsCreated() {
+        String departmentName = "Intellij";
+        Integer departmentId = 2020;
+
+        Department department = Department.builder()
+                .departmentName(departmentName)
+                .departmentId(departmentId)
+                .build();
+
+        Assert.assertEquals(departmentName, department.getDepartmentName());
+        Assert.assertEquals(departmentId, department.getDepartmentId());
+    }
+
+    @Test(expected = NullPointerException.class)
+    @DisplayName("Checks if the departments name is Null")
+    public void CheckIfTheDepartmentNameIsNull(){
+        Department.builder()
+                .departmentName(null)
+                .departmentId(1)
+                .build();
+    }
+
+    @Test(expected = NullPointerException.class)
+    @DisplayName("Checks if the departments Id is Null")
+    public void CheckIfTheDepartmentIdIsNull(){
+        Department.builder()
+                .departmentName("Intellij")
+                .departmentId(null)
+                .build();
     }
 }
