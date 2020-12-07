0122534d4b6bf26618e63674eba10586c2fb5c7d
Shaikh <kashifa.nibras@gmail.com>
2020-12-07 14:01:16 +0100

    added more tests

--- src/test/java/se/jensen/exercise/department/TestDepartmentDatabaseEntryCreated.java
+++ src/test/java/se/jensen/exercise/department/TestDepartmentDatabaseEntryCreated.java
@@ -1,23 +1,17 @@
 package se.jensen.exercise.department;
-
-
 import org.junit.Assert;
 import org.junit.Test;
-import se.jensen.entity.Department;
-
+import se.jensen.dao.DepartmentDatabaseEntry;
 public class TestDepartmentDatabaseEntryCreated {
     @Test
-    public void testDepartmentDatabaseEntryCreated(){
-        Integer departmentId = 1;
-        String departmentName= "newDepartment";
-
-        Department departmentDatabaseEntry = Department.builder()
-                .departmentId(departmentId)
-                .departmentName(departmentName)
+    public void testDepartmentDatabaseEntryCreated()
+    {
+        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
+                .departmentId(1)
+                .departmentName("NewDevelopment")
                 .build();
-
         Assert.assertNotNull(departmentDatabaseEntry);
-        Assert.assertEquals(departmentId, departmentDatabaseEntry.getDepartmentId());
-        Assert.assertEquals(departmentName, departmentDatabaseEntry.getDepartmentName());
+        Assert.assertEquals(Integer.valueOf(1), departmentDatabaseEntry.getDepartmentId());
+        Assert.assertEquals("NewDevelopment", departmentDatabaseEntry.getDepartmentName());
     }
 }
