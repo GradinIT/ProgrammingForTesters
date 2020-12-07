2f69f0f18c4094592a6c4b263542dfde8048226a
Subhashini <sarathy.na@gmail.com>
2020-12-07 13:07:31 +0100

    created

---
+++ src/test/java/se/jensen/exercise/department/DepartmentDaoTest.java
@@ -0,0 +1,34 @@
+package se.jensen.exercise.department;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.jensen.H2JpaConfig;
+import se.jensen.LiquibaseConfigurer;
+import se.jensen.dao.DepartmentDao;
+import se.jensen.dao.mapper.DepartmentDatabaseEntryMapper;
+import se.jensen.entity.Department;
+import se.jensen.exercise.DepartmentTestBuilder;
+
+import javax.ws.rs.core.Application;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class, LiquibaseConfigurer.class, H2JpaConfig.class})
+public class DepartmentDaoTest {
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @Test
+    public void testSaveNewDepartment(){
+        Department department = DepartmentDatabaseEntryMapper.map(departmentDao.save(DepartmentDatabaseEntryMapper.map(
+                DepartmentTestBuilder.build())));
+
+        List<Department> departments = DepartmentDatabaseEntryMapper.map(departmentDao.findAll());
+        Assert.assertNotNull(department);
+    }
+}
