9368f29c208707ce7df473d81b25bf1f9d60b86a
Shaikh <kashifa.nibras@gmail.com>
2020-12-07 09:51:37 +0100

    new employee added

--- src/test/java/se/jensen/exercise/department/TestDepartmentDatabaseEntryCreated.java
+++ src/test/java/se/jensen/exercise/department/TestDepartmentDatabaseEntryCreated.java
@@ -1,7 +1,23 @@
 package se.jensen.exercise.department;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+import se.jensen.entity.Department;
+
 public class TestDepartmentDatabaseEntryCreated {
+    @Test
+    public void testDepartmentDatabaseEntryCreated(){
+        Integer departmentId = 1;
+        String departmentName= "newDepartment";
 
+        Department departmentDatabaseEntry = Department.builder()
+                .departmentId(departmentId)
+                .departmentName(departmentName)
+                .build();
 
+        Assert.assertNotNull(departmentDatabaseEntry);
+        Assert.assertEquals(departmentId, departmentDatabaseEntry.getDepartmentId());
+        Assert.assertEquals(departmentName, departmentDatabaseEntry.getDepartmentName());
+    }
 }
