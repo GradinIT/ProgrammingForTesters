5d86211f7b09639450fa4e2e2e8109b9d03904b4
galstyand <galstyand@gmail.com>
2020-12-08 19:15:38 +0100

    changes that TestDepartmentModelsMapper works with DepartmentTestSuite

--- src/test/java/se/jensen/exercise/department/TestDepartmentModelsMapper.java
+++ src/test/java/se/jensen/exercise/department/TestDepartmentModelsMapper.java
@@ -1,13 +1,16 @@
 package se.jensen.exercise.department;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.Test;
 import se.jensen.api.DepartmentModel;
 import se.jensen.api.mapper.DepartmentModelsMapper;
 import se.jensen.entity.Department;
+import se.jensen.test.category.UnitTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//@Category(UnitTest.class)
 public class TestDepartmentModelsMapper {
     @Test
     public void testDepartmentModelsMapper(){
@@ -17,7 +20,7 @@ public class TestDepartmentModelsMapper {
                 .departmentName("Department1")
                 .build());
         List<DepartmentModel> modelList = DepartmentModelsMapper.map(departmentList);
-        Assert.assertEquals(1,modelList.size());
+        Assert.assertEquals(departmentList.size(), modelList.size());
         Assert.assertEquals(Integer.valueOf(1),modelList.get(0).getDepartmentId());
         Assert.assertEquals("Department1",modelList.get(0).getDepartmentName());
     }
