ecbcb800b3e756a25c2256a4648030c1471880c6
JockeLind <joakimlinderholm@hotmail.com>
2020-12-09 14:29:22 +0100

    testing commit for new pull and not get conflicts

--- src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
+++ src/test/java/se/jensen/exercise/department/TestThatDepartmentIsCreated.java
@@ -1,4 +1,27 @@
 package se.jensen.exercise.department;
 
+import org.junit.Assert;
+import org.junit.Test;
+import se.jensen.entity.Department;
+import se.jensen.entity.Employee;
+
 public class TestThatDepartmentIsCreated {
+
+
+
+    @Test
+    public void TestDepartmentValues_IdAndName() {
+        Integer departmentId = 1;
+        String departmentName = "Department 1";
+
+        Department department = Department.builder()
+                .departmentId(departmentId)
+                .departmentName(departmentName)
+                .build();
+
+        Assert.assertEquals(departmentId, department.getDepartmentId());
+        Assert.assertEquals(departmentName, department.getDepartmentName());
+
+
+    }
 }
--- src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
+++ src/test/java/se/jensen/exercise/department/TestThatNullPointerExceptionIsThrown.java
@@ -1,4 +1,29 @@
 package se.jensen.exercise.department;
 
+import org.junit.Test;
+import se.jensen.entity.Department;
+
+
+
 public class TestThatNullPointerExceptionIsThrown {
+
+    @Test(expected = NullPointerException.class)
+    public void TestDepartmentIdINull() {
+        Department.builder()
+                .departmentId(null)
+                .departmentName("Department 1")
+                .build();
+    }
+    @Test(expected = NullPointerException.class)
+    public void TestDepartmentNameNull() {
+        Department.builder()
+                .departmentId(1)
+                .departmentName(null)
+                .build();
+
+
+    }
+
 }
+
+
