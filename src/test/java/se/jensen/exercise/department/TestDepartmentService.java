34c86da9c2fc8000ce1918ffe4ea6fbc3c6b6f62
MusseJ <73593259+MusseJ@users.noreply.github.com>
2020-12-09 09:26:18 +0100

    Mocked DepartmentService

--- src/test/java/se/jensen/exercise/department/TestDepartmentService.java
+++ src/test/java/se/jensen/exercise/department/TestDepartmentService.java
@@ -1,4 +1,68 @@
 package se.jensen.exercise.department;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import se.jensen.entity.Department;
+import se.jensen.dao.DepartmentDao;
+import se.jensen.dao.DepartmentDatabaseEntry;
+import se.jensen.service.DepartmentService;
+import se.jensen.service.DepartmentServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 public class TestDepartmentService {
+    DepartmentDao departmentDao = mock(DepartmentDao.class);
+    @InjectMocks
+    DepartmentService service = new DepartmentServiceImpl();
+
+    private final String DEPARTMENTNAME = "Intellij";
+    private final Integer DEPARTMENTID = 2020;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
+                .departmentName(DEPARTMENTNAME)
+                .departmentId(2020)
+                .build();
+        List<DepartmentDatabaseEntry> list = new ArrayList<>();
+        list.add(departmentDatabaseEntry);
+
+        when(departmentDao.findAll()).thenReturn(list);
+
+    }
+    @Test
+    @DisplayName("Test To Get All Departments ")
+    public void TestToGetAllDepartments() {
+        List<Department> all = service.getDepartments();
+        verify(departmentDao, times(1)).findAll();
+        Assert.assertNotNull(all);
+        Assert.assertEquals(1, all.size());
+    }
+    @Test
+    @DisplayName("Test To Get Departments By Id")
+    public void TestToGetDepartmentsById() {
+        Integer departmentId = Integer.valueOf(15);
+
+        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder() // setup test reply from database
+                .departmentId(2020)
+                .departmentName(DEPARTMENTNAME)
+                .build();
+
+        when(departmentDao.findById(departmentId)).thenReturn(Optional.of(departmentDatabaseEntry)); // Mock the call
+
+        Department department = service.getDepartmentById(departmentId);
+        verify(departmentDao, times(1)).findById(departmentId);
+        Assert.assertNotNull(department);
+        Assert.assertEquals(DEPARTMENTNAME, department.getDepartmentName());
+        Assert.assertEquals(DEPARTMENTID, department.getDepartmentId());
+    }
 }
