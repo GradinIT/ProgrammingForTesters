f0e82c77ca901a6972868eb214ab796f40d3ffe3
Lakshmi <lakshmijulakanti@gmail.com>
2020-12-14 14:47:16 +0100

    Added more test cases

---
+++ src/test/java/se/jensen/exercise/department/TestDepartment.java
@@ -0,0 +1,27 @@
+package se.jensen.exercise.department;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.jensen.entity.Department;
+
+public class TestDepartment {
+    @Test
+    public void testThatDepartmentIsCreatedCorrectly(){
+        Integer department_Id = 1;
+        String department_Name = "Section A";
+        Department department = Department.builder()
+                .departmentId(department_Id)
+                .departmentName(department_Name)
+                .build();
+        Assert.assertNotNull(department);
+        Assert.assertEquals(department_Id,department.getDepartmentId());
+        Assert.assertEquals(department_Name,department.getDepartmentName());
+    }
+    @Test(expected = NullPointerException.class)
+    public void testNullValueNotAllowedForDepartmentId(){
+        Department.builder()
+                .departmentId(null)
+                .departmentName("Section A")
+                .build();
+    }
+}
