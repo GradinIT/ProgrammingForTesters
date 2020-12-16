f24ae84118b5606aacfc510daba6021e91c9c3fa
Subhashini <sarathy.na@gmail.com>
2020-12-16 16:38:04 +0100

    Added to TestSuite

--- src/test/java/se/jensen/exercise/department/DepartmentTestSuite.java
+++ src/test/java/se/jensen/exercise/department/DepartmentTestSuite.java
@@ -6,6 +6,11 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         TestThatDepartmentIsCreated.class,
+        TestDepartmentDatabaseEntryCreated.class,
+        TestDepartmentDatabaseEntryMapperCreated.class,
+        TestDepartmentModelCreated.class,
+        TestDepartmentModelMapper.class,
+        TestDepartmentModelsMapper.class,
         TestThatNullPointerExceptionIsThrown.class,
         TestThatDepartmentIsStoredInDatabase.class,
         TestDepartmentService.class
