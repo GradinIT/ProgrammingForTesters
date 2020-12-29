dc144a4572cfdcca7452af477d0c366f9c757849
MusseJ <73593259+MusseJ@users.noreply.github.com>
2020-12-29 01:13:38 +0100

    Added Delete in DepartmentService

--- src/test/java/se/jensen/exercise/department/TestDepartmentService.java
+++ src/test/java/se/jensen/exercise/department/TestDepartmentService.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import se.jensen.dao.mapper.DepartmentDatabaseEntryMapper;
 import se.jensen.entity.Department;
 import se.jensen.dao.DepartmentDao;
 import se.jensen.dao.DepartmentDatabaseEntry;
 import se.jensen.exercise.test.builder.DepartmentTestBuilder;
 import se.jensen.service.DepartmentService;
 import se.jensen.service.DepartmentServiceImpl;
-import se.jensen.dao.DepartmentDao;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,14 +26,14 @@ public class TestDepartmentService {
     DepartmentService service = new DepartmentServiceImpl();
 
     private final String DEPARTMENTNAME = "Intellij";
-    private final Integer DEPARTMENTID = 2020;
+    private final Integer DEPARTMENTID = 1;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
                 .departmentName(DEPARTMENTNAME)
-                .departmentId(2020)
+                .departmentId(1)
                 .build();
         List<DepartmentDatabaseEntry> list = new ArrayList<>();
         list.add(departmentDatabaseEntry);
@@ -52,19 +52,35 @@ public class TestDepartmentService {
     @Test
     @DisplayName("Test To Get Departments By Id")
     public void TestToGetDepartmentsById() {
-        Integer departmentId = Integer.valueOf(15);
+        Integer departmentId = Integer.valueOf(1);
 
-        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder() // setup test reply from database
-                .departmentId(2020)
+        DepartmentDatabaseEntry departmentDatabaseEntry = DepartmentDatabaseEntry.builder()
+                .departmentId(1)
                 .departmentName(DEPARTMENTNAME)
                 .build();
 
-        when(departmentDao.findById(departmentId)).thenReturn(Optional.of(departmentDatabaseEntry)); // Mock the call
+        when(departmentDao.findById(departmentId)).thenReturn(Optional.of(departmentDatabaseEntry));
 
         Department department = service.getDepartmentById(departmentId);
         verify(departmentDao, times(1)).findById(departmentId);
         Assert.assertNotNull(department);
         Assert.assertEquals(DEPARTMENTNAME, department.getDepartmentName());
         Assert.assertEquals(DEPARTMENTID, department.getDepartmentId());
+    }
+
+    @Test
+    @DisplayName("Test To Delete Department")
+    public void TestToDeleteDepartment(){
+        List<Department> departmentsBeforeDelete = DepartmentDatabaseEntryMapper.map(departmentDao.findAll());
+        System.out.println("\nDepartment before delete");
+        departmentsBeforeDelete.stream().forEach(System.out::println);
+        departmentDao.delete(DepartmentDatabaseEntryMapper.map(Department.builder()
+                .departmentId(5)
+                .departmentName("Engineer")
+                .build()));
+        List<Department> departmentsAfterDelete = DepartmentDatabaseEntryMapper.map(departmentDao.findAll());
+        System.out.println("Departments after delete");
+        departmentsAfterDelete.stream().forEach(System.out::println);
+        Assert.assertEquals(1,departmentsAfterDelete.size());
     }
 }
--- src/test/java/se/jensen/exercise/test/builder/DepartmentTestBuilder.java
+++ src/test/java/se/jensen/exercise/test/builder/DepartmentTestBuilder.java
@@ -3,10 +3,16 @@ package se.jensen.exercise.test.builder;
 import se.jensen.entity.Department;
 
 public class DepartmentTestBuilder {
+    private static Department.DepartmentBuilder builder = Department.builder();
+
     public static Department build() {
         return Department.builder()
                 .departmentId(1)
                 .departmentName("Development")
                 .build();
+    }
+
+    public static Department.DepartmentBuilder builder() {
+        return builder;
     }
 }
