221306495979d502ec714ee425055086d2e07b71
rosario bustes <rosario.bustes@gmail.com>
2021-01-07 14:04:20 +0100

    RestGetAll

---
+++ src/test/java/se/jensen/exercise/department/client/TestDepartmentRestGetAll.java
@@ -0,0 +1,55 @@
+package se.jensen.exercise.department.client;
+
+import lombok.SneakyThrows;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.jensen.RestServiceApplication;
+import se.jensen.api.DepartmentModel;
+import se.jensen.api.EmployeeModel;
+import se.jensen.exercise.department.client.DepartmentRestServiceClient;
+import se.jensen.exercise.employee.client.EmployeeRestServiceClient;
+import se.jensen.test.category.IntegrationTest;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RestServiceApplication.class})
+@Category(IntegrationTest.class)
+
+public class TestDepartmentRestGetAll {
+
+    private static ConfigurableApplicationContext applicationContext;
+
+    @SneakyThrows
+    @BeforeClass
+    public static void startUp() {
+        String[] args = {};
+        SpringApplication app = new SpringApplication(RestServiceApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8080"));
+        app.setBannerMode(Banner.Mode.CONSOLE);
+        app.setLogStartupInfo(false);
+        applicationContext = app.run(args);
+    }
+    @AfterClass
+    public static void shutDown() {
+        SpringApplication.exit(applicationContext);
+    }
+
+    @Test
+    public void happyFlow() {
+        Optional<List<DepartmentModel>> departmentModels = DepartmentRestServiceClient.getAllDepartments();
+        Assert.assertTrue(departmentModels.isPresent());
+    }
+}
