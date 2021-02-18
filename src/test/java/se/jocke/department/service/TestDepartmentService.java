ebe5479eee347a9c3eebe87e5bd63c67ea0da7a7
jocke <joakim.gradin@gradin-it.com>
2021-02-18 09:24:46 +0100

    adding stuff

--- src/test/java/se/jocke/department/service/TestDepartmentService.java
+++ src/test/java/se/jocke/department/service/TestDepartmentService.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
