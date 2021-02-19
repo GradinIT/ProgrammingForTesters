7a17bb084b5d2d4c11fca6b7fdfaee0ab9bebbc1
Fabian Ekbäck <fabian.ekback@gmail.com>
2021-02-19 14:05:45 +0100

    Looked at Jocke's code and applied it to Employee

--- src/test/java/se/jocke/employee/service/TestEmployeeService.java
+++ src/test/java/se/jocke/employee/service/TestEmployeeService.java
@@ -1,4 +1,61 @@
 package se.jocke.employee.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.jocke.dao.EmployeeDao;
+import se.jocke.dao.EmployeeDatabaseEntry;
+import se.jocke.department.entity.Employee;
+import se.jocke.service.EmployeeService;
+import se.jocke.service.EmployeeServiceImpl;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
 public class TestEmployeeService {
+    @Mock
+    private EmployeeDao employeeDao;
+    @InjectMocks
+    private EmployeeService systemUnderTest = new EmployeeServiceImpl();
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        when(employeeDao.findById(any(Integer.class))).thenReturn(Optional.of(EmployeeDatabaseEntry.builder()
+                .employeeId(1)
+                .firstName("Ludde")
+                .lastName("Ljungman")
+                .salary(BigDecimal.valueOf(40000.0))
+                .fullTime(Boolean.TRUE)
+                .departmentId(34)
+                .build()));
+    }
+
+    @Test
+    public void findByID() {
+        Employee employee = systemUnderTest.getEmployeeById(1);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, employee.getEmployeeId().getId()),
+                () -> Assertions.assertEquals("Ludde", employee.getFirstName()),
+                () -> Assertions.assertEquals("Ljungman", employee.getLastName()),
+                () -> Assertions.assertEquals(BigDecimal.valueOf(40000.0), employee.getSalary()),
+                () -> Assertions.assertEquals(Boolean.TRUE, employee.getFullTime()),
+                () -> Assertions.assertEquals(34, employee.getDepartmentId())
+        );
+        verify(employeeDao,times(1)).findById(1);
+    }
 }
