2258fb32d0f4f086d15d8134dd545cfbf45a8e28
Gustav Gisselfeldt <Gustav@Gustav-no-MacBook-Pro.local>
2021-02-23 14:03:26 +0100

    changed stuff

---
+++ src/test/java/se/jocke/employee/api/model/TestCreateEmployeeModel.java
@@ -0,0 +1,34 @@
+package se.jocke.employee.api.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import se.jocke.api.EmployeeModel;
+import se.jocke.api.mapper.EmployeeModelMapper;
+import se.jocke.employee.builder.EmployeeModelTestBuilder;
+
+public class TestCreateEmployeeModel {
+    private final EmployeeModel EMPLOYEE_MODEL = EmployeeModelTestBuilder.builder().build();
+
+    @Test
+    public void testCreateEmployeeModel() {
+        EmployeeModel employeeModel = EmployeeModelMapper.mapEmployeeModelFromAnotherEmployeeModel(EMPLOYEE_MODEL);
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getEmployeeId(), employeeModel.getEmployeeId()),
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getFirstName(), employeeModel.getFirstName()),
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getLastName(), employeeModel.getLastName()),
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getSalary(), employeeModel.getSalary()),
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getFullTime(), employeeModel.getFullTime()),
+                () -> Assertions.assertEquals(EMPLOYEE_MODEL.getDepartmentId(), employeeModel.getDepartmentId())
+        );
+
+    }
+
+    @Test
+    public void testCreateEmployeeModelThrowsException() {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                EmployeeModel.builder().firstName(EMPLOYEE_MODEL.getFirstName()).build());
+    }
+}
+
