c76b3615b3b45413a8af68af86c9fafa32653832
LindaEsefzara <lindayoukhanna@hotmail.se>
2021-02-24 15:13:17 +0100

    kommentar

--- src/main/java/se/jocke/basic/Calculator.java
+++ src/main/java/se/jocke/basic/Calculator.java
@@ -4,4 +4,12 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
+
+    public int addition(int a, int b){return a+b;}
+
+    public double divide(double a, double b){return a/b;}
+
+    public int subtract(int a, int b){return a-b;}
+
 }
+
--- src/test/java/se/jocke/basic/TestCalculator.java
+++ src/test/java/se/jocke/basic/TestCalculator.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestCalculator {
-    private Calculator calculator = new Calculator();
 
+    private Calculator calculator = new Calculator();
     @Test
     public void testMultiplycation() {
         int product = calculator.multiply(4,2);
@@ -21,4 +21,30 @@ public class TestCalculator {
         int product = calculator.multiply(-4,-2);
         Assertions.assertEquals(8,product);
     }
+    @Test
+    public void testAddition(){
+        int product = calculator.addition(10, 2);
+        Assertions.assertEquals(12,product);
+    }
+    @Test
+    public void testAddition1(){
+        int product1 = calculator.addition(5,5);
+        Assertions.assertEquals(10,product1);
+    }
+    @Test
+    public void testAddition2(){
+        int product2 = calculator.addition(6,8);
+        Assertions.assertEquals(14,product2);
+    }
+    @Test
+    public void testDivide(){
+        double product = calculator.divide(10,2);
+        Assertions.assertEquals(5,product);
+    }
+    @Test
+    public void testDivide1(){
+        double product = calculator.divide(5,2);
+        Assertions.assertEquals(2.5,product);
+    }
+
 }
