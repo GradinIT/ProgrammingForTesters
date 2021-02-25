6e8bfeb11741f09e74b32fd1596447ac7387661d
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-02-25 11:12:24 +0100

    hejo

--- src/test/java/se/jocke/employee/api/model/TestCreateEmployeeModel.java
+++ src/test/java/se/jocke/employee/api/model/TestCreateEmployeeModel.java
@@ -3,6 +3,7 @@ package se.jocke.employee.api.model;
 import io.cucumber.java.bs.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import se.jocke.api.EmployeeModel;
 import se.jocke.department.entity.Employee;
 import se.jocke.employee.builder.EmployeeTestBuilder;
 
@@ -12,12 +13,13 @@ public class TestCreateEmployeeModel {
 
     @Test
     public void testBuildEmployeeModel() {
-        Employee empModel = EmployeeTestBuilder.builder()
+        Employee empModel = Employee.builder()
                 .employeeId(EMPLOYEE_MODEL.getEmployeeId())
                 .firstName(EMPLOYEE_MODEL.getFirstName())
                 .lastName(EMPLOYEE_MODEL.getLastName())
                 .fullTime(EMPLOYEE_MODEL.getFullTime())
                 .salary(EMPLOYEE_MODEL.getSalary())
+                .departmentId(EMPLOYEE_MODEL.getDepartmentId())
                 .build();
 
                 Assertions.assertEquals(EMPLOYEE_MODEL.getEmployeeId(),empModel.getEmployeeId());
@@ -25,6 +27,7 @@ public class TestCreateEmployeeModel {
                 Assertions.assertEquals(EMPLOYEE_MODEL.getLastName(),empModel.getLastName());
                 Assertions.assertEquals(EMPLOYEE_MODEL.getFullTime(),empModel.getFullTime());
                 Assertions.assertEquals(EMPLOYEE_MODEL.getSalary(),empModel.getSalary());
+                Assertions.assertEquals(EMPLOYEE_MODEL.getDepartmentId(),empModel.getDepartmentId());
                 Assertions.assertEquals(EMPLOYEE_MODEL,empModel);
     }
 }
