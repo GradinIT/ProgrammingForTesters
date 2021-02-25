17f1ef7f6ce30942f159ba8ef601f7543fa81bec
Fredrik Lindström <fredrik_lindstrom@live.com>
2021-02-25 14:16:55 +0100

    latest

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,4 +1,40 @@
 package se.jocke.employee.integrationtest;
 
-public class TestEmployeeRestAPI {
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import se.jocke.TestClient;
+import se.jocke.api.EmployeeModel;
+import se.jocke.api.EmployeeModel;
+import se.jocke.api.EmployeeModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TestEmployeeRestAPI extends TestClient {
+
+    Optional<List<EmployeeModel>> employees = null;
+    Optional<EmployeeModel> employee = null;
+
+
+    @When("^the client calls /employee$")
+    public void getAll() throws Throwable {
+        employees = getAllEmployees();
+    }
+
+    @Then("^the client receives (\\d+) employees$")
+    public void theClientGotAllEmployees(int numberOfEmployees) throws Throwable {
+        Assert.assertEquals(numberOfEmployees, employees.get().size());
+    }
+
+    @When("^the client updates firstname for employee to (.+)$")
+    public void updateNameOfEmployee(String employeeName) throws Throwable {
+        updateEmployee(EmployeeModel.builder().employeeId(1).firstName(employeeName).build());
+    }
+
+    @Then("the firstname is updated to (.+)$")
+    public void nameOfEmployeeIsUpdated(String employeeName) throws Throwable {
+        Optional<EmployeeModel> employee = getEmployeeById(1);
+        Assert.assertEquals(employeeName, employee.get().getFirstName());
+    }
 }
