c70b026167d022fbd443b4198f371e02da1a8a90
Mona Karlsson <mona.m.karlsson@gmail.com>
2021-02-25 13:12:16 +0100

    kommentar

---
+++ src/test/java/se/jocke/employee/api/model/TestCreateEmployeeModel.java
@@ -0,0 +1,30 @@
+package se.jocke.department.api.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import se.jocke.api.DepartmentModel;
+import se.jocke.department.builder.DepartmentModelTestBuilder;
+
+public class TestCreateDepartmentModel {
+    private final DepartmentModel DEPARTMENT_MODEL = DepartmentModelTestBuilder.builder().build();
+
+
+    @Test
+    public void testCreateDepartmentModel() {
+        DepartmentModel department = DepartmentModel.builder()
+                .departmentId(DEPARTMENT_MODEL.getDepartmentId())
+                .departmentName(DEPARTMENT_MODEL.getDepartmentName())
+                .build();
+
+        Assertions.assertEquals(DEPARTMENT_MODEL.getDepartmentName(), department.getDepartmentName());
+        Assertions.assertEquals(DEPARTMENT_MODEL.getDepartmentId(), department.getDepartmentId());
+        Assertions.assertEquals(DEPARTMENT_MODEL,department);
+    }
+
+    @Test
+    public void testCreateDepartmentModelThrowsException() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            DepartmentModel.builder().departmentName(DEPARTMENT_MODEL.getDepartmentName()).build();
+        });
+    }
+}
