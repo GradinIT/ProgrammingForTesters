ffa0179ad02be46e34516c20b667d0479b650194
LuddeLjungman <luddeljungman@gmail.com>
2021-02-26 12:42:16 +0100

    Done stuff in employeeTest.feature, added TestEmployeeRestAPI and EmployeeIntegrationTest

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,4 +1,24 @@
 package se.jocke.employee.integrationtest;
 
-public class TestEmployeeRestAPI {
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import se.jocke.TestClient;
+import se.jocke.api.EmployeeModel;
+
+import java.util.List;
+import java.util.Optional;
+
+public class TestEmployeeRestAPI extends TestClient {
+    Optional<List<EmployeeModel>> employees = null;
+    Optional<EmployeeModel> employee = null;
+
+    @When("^the client calls /employee$")
+    public void getAll() throws Throwable {
+        employees = getAllEmployees();
+    }
+    @Then("^the client receives {int} employees$")
+    public void theClientGotAllEmployees(int numberOfEmployees) throws Throwable {
+        Assert.assertEquals(numberOfEmployees, employees.get().size());
+    }
 }
