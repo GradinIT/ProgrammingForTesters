d6dfd929ab0d00a4bcaa876b18768ecf16245258
Alexander Frödin <alexanderdelaval@gmail.com>
2021-02-26 11:52:46 +0100

    Added some more tests to integrationtest employee

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,31 +1,87 @@
 package se.jocke.employee.integrationtest;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.DataTableType;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import se.jocke.TestClient;
 import se.jocke.api.EmployeeModel;
-import se.jocke.api.mapper.EmployeeModelMapper;
-import se.jocke.dao.mapper.EmployeePojoMapper;
 import se.jocke.department.entity.Employee;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class TestEmployeeRestAPI extends TestClient {
 
-    Optional<List<EmployeeModel>> employeeModelOptional = null;
-    Optional<List<Employee>> employeeOptional = null;
+    Optional<List<EmployeeModel>> employees = null;
+    Optional <EmployeeModel> employee = null;
 
     @When("^the client calls /employee$")
     public void getAll() {
-        employeeModelOptional = getAllEmployees();
+        employees = getAllEmployees();
     }
 
     @Then("the client receives {int} employees")
     public void theClientGetAllEmployees(Integer numOfEmployees) {
-        Assertions.assertEquals(numOfEmployees, employeeModelOptional.get().size());
+        Assertions.assertEquals(numOfEmployees, employees.get().size());
+    }
+
+    @When("the client request employee id {int}")
+    public void employeeById(Integer employeeId) {
+        employee = getEmployeeById(employeeId);
+    }
+
+    @Then("the client get employee {int}")
+    public void employeeNameIs(Integer employeeId) {
+        Assertions.assertEquals(employeeId, employee.get().getEmployeeId());
+    }
+
+    @When("the client updates name for employee {int} to {}")
+    public void updateNameOfEmployee(Integer employeeId, String name) {
+        employee = getEmployeeById(employeeId);
+
+        employee = updateEmployee(EmployeeModel.builder()
+                .employeeId(employee.get().getEmployeeId())
+                .firstName(name)
+                .lastName(employee.get().getLastName())
+                .fullTime(employee.get().getFullTime())
+                .salary(employee.get().getSalary())
+                .departmentId(employee.get().getDepartmentId())
+                .build()
+        );
+    }
+
+    @Then("the name of employee is updated to {}")
+    public void theNameIsUpdatedTo(String name) {
+        Assertions.assertEquals(name, employee.get().getFirstName());
+    }
+
+    @Given("^the employees$")
+    public void createEmployees(DataTable employees) {
+        List<EmployeeModel> listOfEmployees = makeList(employees.asList());
+        listOfEmployees.stream().forEach(TestClient::createEmployee);
+    }
+
+    private List<EmployeeModel> makeList(List<String> emp) {
+        List<EmployeeModel> employeeModels = new ArrayList<>();
+
+        for (int i = 0; i < emp.size(); i += 6) {
+            employeeModels.add(
+                    EmployeeModel.builder()
+                    .employeeId(Integer.parseInt(emp.get(i)))
+                    .firstName(emp.get(i +1))
+                    .lastName(emp.get(i + 2))
+                    .fullTime(Boolean.valueOf(emp.get(i +3)))
+                    .salary(new BigDecimal(emp.get(i + 4)))
+                    .departmentId(Integer.parseInt(emp.get(i + 5)))
+                    .build()
+            );
+        }
+        return employeeModels;
     }
 
 }
--- src/test/resources/employee/employeeTest.feature
+++ src/test/resources/employee/employeeTest.feature
@@ -2,3 +2,14 @@ Feature: Test employee API
   Scenario: client gets all employees
     When the client calls /employee
     Then the client receives 3 employees
+  Scenario: the client get employee by id
+    When the client request employee id 1
+    Then the client get employee 1
+  Scenario: client updates name for employee 1
+    When the client updates name for employee 1 to Alexander
+    Then the name of employee is updated to Alexander
+  Scenario: client create employees
+    Given the employees
+    | 100 | Alexander | Frodi | true | 55000 | 1 |
+    | 101 | Joakim | Gradin | true | 65000 | 2 |
+    | 100 | Linus | Hellberg | true | 55000 | 3 |
