e17e0be815ec7deeca1c77cfddc05e414abb097c
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-02-27 18:46:50 +0100

    Kommentar

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -5,9 +5,9 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import se.jocke.TestClient;
-import se.jocke.api.DepartmentModel;
 import se.jocke.api.EmployeeModel;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,44 +17,55 @@ public class TestEmployeeRestAPI extends TestClient {
 
 
     @When("^the client calls /employee$")
-    public void getAll() throws Throwable {
+    public void getAll(){
         employees = getAllEmployees();
     }
 
     @Then("^the client receives (\\d+) employees$")
-    public void theClientGotAllEmployees(int numberOfEmployees) throws Throwable {
+    public void theClientGotAllEmployees(int numberOfEmployees){
         Assert.assertEquals(numberOfEmployees, employees.get().size());
     }
 
     @When("^the client updates first name for employee to (.+)$")
-    public void updateFirstNameOfEmployee(String employeeName) throws Throwable {
+    public void updateFirstNameOfEmployee(String employeeName){
         updateEmployee(EmployeeModel.builder().employeeId(1).firstName(employeeName).build());
     }
 
-    @Then("^the name is updated to (.+)$")
-    public void nameOfEmployeeIsUpdated(String employeeName) throws Throwable {
+    @Then("^the first name is updated to (.+)$")
+    public void nameOfEmployeeIsUpdated(String employeeName){
         Optional<EmployeeModel> employee = getEmployeeById(1);
         Assert.assertEquals(employeeName, employee.get().getFirstName());
     }
-    @When("^the client gets department (\\d+)$")
-    public void getTheEmployeeById(Integer employeeId) throws Throwable {
+    @When("^the client gets employee (\\d+)$")
+    public void getTheEmployeeById(Integer employeeId){
         employee = getEmployeeById(employeeId);
     }
 
-    @Then("^the name is$")
-    public void nameOfEmployeesFirstNameIs() throws Throwable {
-        Assert.assertEquals("Coding", employee.get().getFirstName());
+    @Then("^the first name is$")
+    public void nameOfEmployeesFirstNameIs(){
+        Assert.assertEquals("Johanna", employee.get().getFirstName());
     }
 
-    @When("^the client updates name for employee to (.+)$")
-    public void updateNameOfEmployeeTo(String employeeName) throws Throwable{
+    @When("^the client updates last name for employee to (.+)$")
+    public void updateNameOfEmployeeTo(String employeeName){
         updateEmployee(EmployeeModel.builder().employeeId(1).lastName(employeeName).build());
     }
 
-    @Then("^the name is updated to (.+)$")
-    public void lastNameOfEmployeeIsUpdated(String employeeName) throws Throwable{
+    @Then("^the last name is updated to (.+)$")
+    public void lastNameOfEmployeeIsUpdated(String employeeName){
         Optional<EmployeeModel> employee = getEmployeeById(1);
-        Assertions.assertEquals(employeeName, employee.get().getFirstName());
+        Assertions.assertEquals(employeeName, employee.get().getLastName());
+    }
+
+    @When("^the client updates salary for employee to (.+)$")
+    public void updateSalaryOfEmployeeTo(BigDecimal salary){
+        updateEmployee(EmployeeModel.builder().employeeId(1).salary(salary).build());
+    }
+
+    @Then("^the salary is updated to (.+)$")
+    public void salaryOfEmployeeIsUpdated(BigDecimal salary){
+        Optional<EmployeeModel> employee = getEmployeeById(1);
+        Assertions.assertEquals(salary, employee.get().getSalary());
     }
 
 }
--- src/test/resources/employee/employeeTes.feature
+++ src/test/resources/employee/employeeTes.feature
@@ -3,11 +3,15 @@ Feature: test employee api
     When the client calls /employee
     Then the client receives 3 employees
   Scenario: client updates first name for employee 1
-    When the client updates first name for employee to Coding
-    Then the name is updated to Coding
+    When the client updates first name for employee to Johanna
+    Then the first name is updated to Johanna
+  Scenario: client updates last name for employee 1
+    When the client updates last name for employee to Employee
+    Then the last name is updated to Employee
+  Scenario: client updates salary for employee 1
+    When the client updates salary for employee to 30000
+    Then the salary is updated to 30000
   Scenario: client gets Employee 1
     When the client gets employee 1
-    Then the name is
-  Scenario: client updates name for employee 1
-    When the client updates name for employee to Employee
-    Then the name is updated to Employee
+    Then the first name is
+
