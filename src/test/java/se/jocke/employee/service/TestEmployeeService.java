7da8cd56de606ccb65a4b6e523a64eb9b10eea0c
Alexander Frödin <alexanderdelaval@gmail.com>
2021-03-01 20:31:02 +0100

    Added some more tests

--- src/test/java/se/jocke/employee/service/TestEmployeeService.java
+++ src/test/java/se/jocke/employee/service/TestEmployeeService.java
@@ -14,6 +14,8 @@ import se.jocke.employee.builder.EmployeeTestBuilder;
 import se.jocke.service.EmployeeService;
 import se.jocke.service.EmployeeServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -77,5 +79,44 @@ public class TestEmployeeService {
                 () -> assertEquals(testEmp.getDepartmentId(), tempEmp.getDepartmentId())
         );
         verify(employeeDao, times(1)).save(any(EmployeeDatabaseEntry.class));
+    }
+
+    @Test
+    public void testRemoveEmployee() {
+        SYSTEM_UNDER_TEST.removeEmployee(testEmp);
+        verify(employeeDao, times(1)).delete(any(EmployeeDatabaseEntry.class));
+    }
+
+    @Test
+    public void testUpdateEmployee() {
+        when(employeeDao.save(any(EmployeeDatabaseEntry.class))).thenReturn(empDbE);
+
+        tempEmp = SYSTEM_UNDER_TEST.updateEmployee(testEmp);
+
+        Assertions.assertAll(
+                () -> assertEquals(testEmp.getEmployeeId().getId(), tempEmp.getEmployeeId().getId()),
+                () -> assertEquals(testEmp.getFirstName(), tempEmp.getFirstName()),
+                () -> assertEquals(testEmp.getLastName(), tempEmp.getLastName()),
+                () -> assertEquals(testEmp.getSalary(), tempEmp.getSalary()),
+                () -> assertEquals(testEmp.getFullTime(), tempEmp.getFullTime()),
+                () -> assertEquals(testEmp.getDepartmentId(), tempEmp.getDepartmentId())
+        );
+        verify(employeeDao, times(1)).save(any(EmployeeDatabaseEntry.class));
+    }
+
+    @Test
+    public void testGetAllEmployees() {
+        List<EmployeeDatabaseEntry> employees = new ArrayList<>();
+        employees.add(empDbE);
+        employees.add(empDbE);
+        employees.add(empDbE);
+
+        when(employeeDao.findAll()).thenReturn(employees);
+
+        List<Employee> tempEmpList = SYSTEM_UNDER_TEST.getAllEmployees();
+
+        Assertions.assertEquals(3, tempEmpList.size());
+
+        verify(employeeDao, times(1)).findAll();
     }
 }
