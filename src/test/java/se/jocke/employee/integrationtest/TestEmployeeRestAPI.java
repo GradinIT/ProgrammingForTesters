0261bf3628c1d5e4a7da454a445fed2cbaf1ed97
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-03-01 10:11:46 +0100

    Created Classes, Current status Isu with getAllEmployees() in testClient() fixed Mock for Employee.

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,4 +1,39 @@
 package se.jocke.employee.integrationtest;
 
-public class TestEmployeeRestAPI {
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.HttpClientErrorException;
+import se.jocke.TestClient;
+import se.jocke.api.EmployeeModel;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestEmployeeRestAPI extends TestClient{
+    Optional<List<EmployeeModel>> employees = null;
+    Optional<EmployeeModel> employee = null;
+
+@When("^Client calls for/Employee$")
+    public void getAll() throws Throwable {
+    //employees.getAllEmployees();
+}
+    @Then("^the client receives (\\d+) employees$")
+            public void theClientGotAllEmployees(int numberOfEmployees) throws Throwable{
+                Assert.assertEquals(numberOfEmployees, employees.get().size());
+    }
+
+@When("^the clinent updated name for employee to (.+)$")
+    public void nameOfEmployeeUpdated() throws Throwable{
+
+}
 }
