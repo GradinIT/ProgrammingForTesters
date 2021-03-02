fe21be6ff42ab48c2afab0a51a17b9170c0564fa
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-03-02 09:52:29 +0100

    integrationstest employee

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,13 +1,23 @@
 package se.jocke.employee.integrationtest;
 
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.messages.Messages;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.web.client.HttpClientErrorException;
 import se.jocke.TestClient;
 import se.jocke.api.EmployeeModel;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestEmployeeRestAPI extends TestClient {
 
@@ -15,7 +25,7 @@ public class TestEmployeeRestAPI extends TestClient {
     Optional<EmployeeModel> employee = null;
 
     @When("^the client calls all employees$")
-            public void getAll() throws Throwable {
+            public void getAll() {
                 employees = getAllEmployees();
             };
 
@@ -32,5 +42,46 @@ public class TestEmployeeRestAPI extends TestClient {
     @Then("^the client receives name (.+)$")
             public void receviedEmployee(String name) throws Throwable {
                 Assert.assertEquals(name,employee.get().getFirstName());
+    }
+
+    @Given("^the employees$")
+            public void givenEmployees(DataTable employees) {
+                List<EmployeeModel> modelEmployees = makeEmployeeList(employees.asList());
+                modelEmployees.stream().forEach(E -> createEmployee(E));
+    }
+
+    @When("^the client deletes employee with ID (\\d+)$")
+            public void deleteEmployee(Integer employeeID) {
+                deleteEmployee(getEmployeeById(employeeID).get());
+    }
+
+    Throwable exceptionThatWasThrown;
+
+    @Then("the employee {int} is deleted")
+    public void employeeIsDeleted(Integer employeeId) {
+        exceptionThatWasThrown = assertThrows(HttpClientErrorException.class, () -> {
+            getEmployeeById(employeeId);
+        });
+    }
+
+    @And("the error message is {int} : [Employee with id {int} not found]")
+    public void checkErrorMessage(Integer errorCode, Integer employeeId) {
+        Assertions.assertEquals(errorCode + " : [Entity with id " + employeeId + " not found]", exceptionThatWasThrown.getMessage());
+    }
+
+    private List<EmployeeModel> makeEmployeeList(List<String> given) {
+        List<EmployeeModel> empModels = new ArrayList<>();
+        for(int i = 0; i < given.size(); i += 6) {
+            empModels.add(EmployeeModel.builder()
+                    .employeeId(Integer.parseInt(given.get(i)))
+                    .firstName(given.get(i + 1))
+                    .lastName(given.get(i + 2))
+                    .fullTime(Boolean.valueOf(given.get(i)))
+                    .salary(new BigDecimal(given.get(i + 4)))
+                    .departmentId(Integer.parseInt(given.get(i + 5)))
+                    .build());
+        }
+
+        return empModels;
     }
 }
--- src/test/resources/employee/employeeTest.feature
+++ src/test/resources/employee/employeeTest.feature
@@ -6,3 +6,10 @@ Feature: test employee api
     Scenario: client get employee by id
         When the client calls employee by id 4
         Then the client receives name Linda
+
+    Scenario: client get employees
+        Given the employees
+            | 10 | Linus | Hellberg | false | 20000.00 | 3 |
+        When the client deletes employee with ID 10
+        Then the employee 10 is deleted
+        And the error message is 404 : [Employee with id 10 not found]
