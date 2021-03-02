f3144b3426f5c1c2c9e78206d2c9241c83e79cf5
Miranda <miranda.friso@stud.sti.se>
2021-03-02 09:50:23 +0100

    IntegrationTest

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -1,4 +1,76 @@
 package se.jocke.employee.integrationtest;
 
-public class TestEmployeeRestAPI {
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.web.client.HttpClientErrorException;
+import se.jocke.TestClient;
+import se.jocke.api.EmployeeModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertThrows;
+
+public class TestEmployeeRestAPI extends TestClient {
+    Optional<List<EmployeeModel>> employees = null;
+    Optional<EmployeeModel> employee = null;
+
+    @When("^the client calls /employee$")
+    public void getAll() throws Throwable {
+        employees = getAllEmployees();
+    }
+    @Then("^the client receives (\\d+) employees$")
+    public void theClientGotAllEmployees(int numberOfEmployees) throws Throwable {
+        Assert.assertEquals(numberOfEmployees, employees.get().size());
+    }
+    @When("^the client updates firstname for employee to (.+)$")
+    public void updateFirstNameOfEmployee(String firstName) throws Throwable {
+        updateEmployee(EmployeeModel.builder().employeeId(1).firstName(firstName).build());
+    }
+    @Then("the firstname is updated to (.+)$")
+    public void firstNameOfEmployeeIsUpdated(String firstName) throws Throwable {
+        Optional<EmployeeModel> employee = getEmployeeById(1);
+        Assert.assertEquals(firstName, employee.get().getFirstName());
+    }
+    @When("^the client gets employee (\\d+)$")
+    public void getTheEmployeeById(Integer employeeId) throws Throwable {
+        employee = getEmployeeById(employeeId);
+    }
+    @Then("^the firstname is$")
+    public void nameOfEmployeeIs() throws Throwable {
+        Assert.assertEquals("Coding", employee.get().getFirstName());
+    }
+    @Given("^the employees$")
+    public void givenEmployees(DataTable employees) {
+        List<EmployeeModel> listOfEmployees = makeEmployeeList(employees.asList());
+        listOfEmployees.stream().forEach(employee -> createEmployee(employee));
+    }
+    private List<EmployeeModel> makeEmployeeList(List<String> given) {
+        List<EmployeeModel> deps = new ArrayList<>();
+        for (int i = 0; i < given.size() - 1; i += 2) {
+            deps.add(EmployeeModel.builder().employeeId(Integer.parseInt(given.get(i))).firstName(given.get(i + 1)).build());
+        }
+        return deps;
+    }
+    @When("the client deletes employee {int}")
+    public void deleteEmployee(Integer employeeId) {
+        deleteEmployee(getEmployeeById(employeeId).get());
+    }
+    Throwable exceptionThatWasThrown;
+    @Then("the employee {int} is deleted")
+    public void employeeIsDeleted(Integer employeeId) {
+        exceptionThatWasThrown = assertThrows(HttpClientErrorException.class, () -> {
+            getEmployeeById(employeeId);
+        });
+    }
+    @And("the error message is {int} : [Entity with id {int} not found]")
+    public void checkErrorMessage(Integer errorCode, Integer employeeId) {
+        Assertions.assertEquals(errorCode + " : [Entity with id " + employeeId + " not found]", exceptionThatWasThrown.getMessage());
+    }
 }
