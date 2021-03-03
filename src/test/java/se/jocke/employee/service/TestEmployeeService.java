870d897ed55e34c2bea4dc692720aff47c1518c6
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-03-03 09:14:36 +0100

    Kommentar

--- src/test/java/se/jocke/employee/service/TestEmployeeService.java
+++ src/test/java/se/jocke/employee/service/TestEmployeeService.java
@@ -1,5 +1,7 @@
 package se.jocke.employee.service;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +9,8 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import se.jocke.dao.EmployeeDao;
 import se.jocke.dao.EmployeeDatabaseEntry;
 import se.jocke.department.entity.Employee;
@@ -16,15 +20,19 @@ import se.jocke.service.EmployeeService;
 import se.jocke.service.EmployeeServiceImpl;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class TestEmployeeService {
     @Mock
     private EmployeeDao employeeDao;
+    @Mock
+    private Employee e;
     @InjectMocks
     private EmployeeService systemUnderTest = new EmployeeServiceImpl();
 
@@ -39,6 +47,11 @@ public class TestEmployeeService {
                 .departmentId(1)
                 .build()));
     }
+/*
+    @BeforeEach
+    public void setUpGetAll(){
+        when(employeeDao.findAll()).thenReturn(List<EmployeeDao>);
+    }*/
 
     @Test
     public void findById() {
@@ -53,4 +66,58 @@ public class TestEmployeeService {
         );
         verify(employeeDao, times(1)).findById(1);
     }
+
+    @Test
+    public void createOrUpdateEmployee(){
+        when(e.getEmployeeId().getId()).thenReturn(5);
+        when(e.getFirstName()).thenReturn("firstName1");
+        when(e.getLastName()).thenReturn("lastName2");
+        when(e.getFullTime()).thenReturn(true);
+        when(e.getSalary()).thenReturn(BigDecimal.valueOf(25000));
+        when(e.getDepartmentId()).thenReturn(1);
+        Employee employee = systemUnderTest.createOrUpdateEmployee(e);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(5, employee.getEmployeeId().getId()),
+                () -> Assertions.assertEquals("firstName1", employee.getFirstName()),
+                () -> Assertions.assertEquals("lastName1", employee.getLastName()),
+                () -> Assertions.assertEquals(Boolean.TRUE, employee.getFullTime()),
+                () -> Assertions.assertEquals(BigDecimal.valueOf(25000), employee.getSalary()),
+                () -> Assertions.assertEquals(1, employee.getDepartmentId())
+        );
+        verify(employeeDao, times(1)).findById(1);
+    }
+
+    @Test
+    public void remove(){
+        Employee employee = systemUnderTest.removeEmployee(systemUnderTest.getEmployeeById(1));
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(1, employee.getEmployeeId().getId()),
+                () -> Assertions.assertEquals("firstName1", employee.getFirstName()),
+                () -> Assertions.assertEquals("lastName1", employee.getLastName()),
+                () -> Assertions.assertEquals(BigDecimal.valueOf(25000), employee.getSalary()),
+                () -> Assertions.assertEquals(Boolean.TRUE, employee.getFullTime()),
+                () -> Assertions.assertEquals(1, employee.getDepartmentId())
+        );
+        verify(employeeDao, times(1)).findById(1);
+    }
+
+    @Test
+    public void update(){
+
+    }
+
+    @Test
+    public void getAll(){
+        List<Employee> employees = systemUnderTest.getAllEmployees();
+        verify(employeeDao, times(1)).findAll();
+    }
+
+/*    Employee createOrUpdateEmployee(Employee employee);
+
+    Employee removeEmployee(Employee employee);
+
+    Employee updateEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();*/
+
 }
--- src/test/resources/employee/employeeTes.feature
+++ src/test/resources/employee/employeeTes.feature
@@ -2,15 +2,19 @@ Feature: test employee api
   Scenario: client gets all employee
     When the client calls /employee
     Then the client receives 3 employees
+
   Scenario: client updates first name for employee 1
     When the client updates first name for employee to Johanna
     Then the first name is updated to Johanna
+    Måste ändra tillbaka employee
   Scenario: client updates last name for employee 1
     When the client updates last name for employee to Employee
     Then the last name is updated to Employee
+
   Scenario: client updates salary for employee 1
     When the client updates salary for employee to 30000
     Then the salary is updated to 30000
+
   Scenario: client gets Employee 1
     When the client gets employee 1
     Then the first name is
