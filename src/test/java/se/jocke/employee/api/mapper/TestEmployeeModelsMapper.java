cdc395e9fff9664b1114a6ea42999af82144dddb
Alexander Frödin <alexanderdelaval@gmail.com>
2021-03-04 16:23:52 +0100

    added test for employee models mapper

---
+++ src/test/java/se/jocke/employee/api/mapper/TestEmployeeModelsMapper.java
@@ -0,0 +1,31 @@
+package se.jocke.employee.api.mapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import se.jocke.api.EmployeeModel;
+import se.jocke.api.mapper.EmployeeModelsMapper;
+import se.jocke.department.entity.Employee;
+import se.jocke.employee.builder.EmployeeTestBuilder;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Test employee models mapper list")
+public class TestEmployeeModelsMapper {
+
+    @Test
+    @DisplayName("Given that we want to map a list of employees to employee models")
+    public void testEmployeeModelsMapper() {
+        List<Employee> employees = Collections.singletonList(EmployeeTestBuilder.builder().build());
+        List<EmployeeModel> employeeModelList = EmployeeModelsMapper.map(employees);
+
+        Assertions.assertAll(
+                () -> assertFalse(employeeModelList.isEmpty()),
+                () -> assertNotNull(employeeModelList),
+                () -> assertEquals(1, employeeModelList.size())
+        );
+    }
+}
