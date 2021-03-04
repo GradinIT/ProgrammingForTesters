5816edd03670935cc7a4dd1985b6b369bfeaba9d
Lahey <oskar.franck@gmail.com>
2021-03-04 14:11:31 +0100

    RestApiTestingDone

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -5,9 +5,11 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.messages.Messages;
-import liquibase.pro.packaged.T;
+import liquibase.pro.packaged.S;
+import org.checkerframework.checker.nullness.Opt;
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.web.client.HttpClientErrorException;
 import se.jocke.TestClient;
 import se.jocke.api.EmployeeModel;
 
@@ -15,6 +17,8 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestEmployeeRestAPI extends TestClient {
     Optional<List<EmployeeModel>> employees = null;
@@ -32,21 +36,28 @@ public class TestEmployeeRestAPI extends TestClient {
 
     @When("^the client updates name for employee to (.+)$")
     public void updateFirstNameOfEmployee(String employeeName) throws Throwable{
-        updateEmployee(EmployeeModel.builder().employeeId(1).firstName(employeeName).build());
+        updateEmployee(EmployeeModel.builder()
+                .employeeId(1)
+                .firstName(employeeName)
+                .lastName("lastName1")
+                .salary(BigDecimal.valueOf(25000))
+                .fullTime(true)
+                .departmentId(1)
+                .build());
     }
 
-    @Then("^the name is updated to (.+)$")
+    @Then("the employees name is updated to (.+)$")
     public void firstNameOfEmployeeIsUpdated(String employeeName) throws Throwable{
         Optional<EmployeeModel> employee = getEmployeeById(1);
         Assert.assertEquals(employeeName, employee.get().getFirstName());
     }
 
-    @When("^the client gets department (\\d+)$")
+    @When("^the client gets employee (\\d+)$")
     public void getTheEmployeeById(Integer employeeId) throws Throwable{
         employee = getEmployeeById(employeeId);
     }
 
-    @Then("^the name is$")
+    @Then("^the employees name is$")
     public void nameOfEmployeeIs() throws Throwable{
         Assert.assertEquals("Egon", employee.get().getFirstName());
     }
@@ -54,13 +65,14 @@ public class TestEmployeeRestAPI extends TestClient {
     @Given("^the employees$")
     public void givenEmployees(DataTable employees) {
         List<EmployeeModel> listOfEmployees = makeEmployeeList(employees.asList());
-        listOfEmployees.stream().forEach(TestClient::createEmployee);
+        listOfEmployees.stream().forEach(employee -> createEmployee(employee));
     }
 
     private List<EmployeeModel> makeEmployeeList(List<String> given) {
         List<EmployeeModel> employees = new ArrayList<>();
         for (int i = 0; i < given.size() -1; i += 6) {
-            employees.add(EmployeeModel.builder().employeeId(Integer.parseInt(given.get(i)))
+            employees.add(EmployeeModel.builder()
+                    .employeeId(Integer.parseInt(given.get(i)))
                     .firstName(given.get(i +1))
                     .lastName(given.get(i + 2))
                     .salary(BigDecimal.valueOf(Long.parseLong(given.get(i + 3))))
@@ -70,16 +82,23 @@ public class TestEmployeeRestAPI extends TestClient {
         }
         return employees;
     }
-    @When()
-    public void deleteEmployee() {
 
+    @When("the client deletes employee {int}")
+    public void deleteEmployee(Integer employeeId) {
+        deleteEmployee(getEmployeeById(employeeId).get());
     }
-    @Then()
-    public void employeeIsDeleted() {
 
+    Throwable exceptionThrown;
+
+    @Then("the employee {int} is deleted")
+    public void employeeIsDeleted(Integer employeeId) {
+        exceptionThrown = assertThrows(HttpClientErrorException.class, () -> {
+            getEmployeeById(employeeId);
+        });
     }
-    @And()
-    public void checkErrorMessage() {
 
+    @And("the employee error message is {int} : [Entity with id {int} not found]")
+    public void checkEmployeeErrorMessage(Integer errorCodes, Integer employeeId) {
+        Assertions.assertEquals(errorCodes + " : [Entity with id " + employeeId + " not found]", exceptionThrown.getMessage());
     }
 }
--- src/test/resources/employee/employeeTest.feature
+++ src/test/resources/employee/employeeTest.feature
@@ -4,7 +4,20 @@ Feature: test employee api
     Then the client receives 3 employees
   Scenario: client updates firstname for employee 1
     When the client updates name for employee to Egon
-    Then the name updated to Egon
+    Then the employees name is updated to Egon
   Scenario: client gets employee 1
     When the client gets employee 1
-    Then the name is
+    Then the employees name is
+  Scenario: client updated firstname for employee 1
+    When the client updates name for employee to Kajsa
+    Then the employees name is updated to Kajsa
+  Scenario: client deletes employee
+    Given the employees
+      | 10 | oskar | franck | 25000 | true | 1 |
+      | 11 | sandra | bjarevall | 25000 | true | 1 |
+    When the client deletes employee 10
+    When the client deletes employee 11
+    Then the employee 10 is deleted
+    And the employee error message is 404 : [Entity with id 10 not found]
+    Then the employee 11 is deleted
+    And the employee error message is 404 : [Entity with id 11 not found]
