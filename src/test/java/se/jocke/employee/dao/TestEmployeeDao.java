805e28f7c44f33a7dcf190b32a51418c70df500d
Oscar Bergström <oscar@obstrom.com>
2021-03-04 20:46:10 +0100

    Added test of EmployeeDao

---
+++ src/test/java/se/jocke/employee/dao/TestEmployeeDao.java
@@ -0,0 +1,44 @@
+package se.jocke.employee.dao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import se.jocke.H2JpaConfig;
+import se.jocke.LiquibaseConfigurer;
+import se.jocke.dao.EmployeeDao;
+import se.jocke.dao.EmployeeDatabaseEntry;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by Oscar Bergström 02-03-2021
+ */
+
+// Start a Spring Boot context for testing a Liquibase dependent component
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {LiquibaseConfigurer.class, H2JpaConfig.class})
+public class TestEmployeeDao {
+
+    // Dependency inject EmployeeDao
+    @Autowired
+    EmployeeDao employeeDao;
+
+    // Test that the database service contains an Employee matching what we expect for Employee #1
+    @Test
+    public void testGetEmployeeId() {
+        Optional<EmployeeDatabaseEntry> optionalEmployeeDatabaseEntry = employeeDao.findById(1);
+        Assertions.assertAll(
+                () -> assertTrue(optionalEmployeeDatabaseEntry.isPresent()),
+                () -> assertNotNull(optionalEmployeeDatabaseEntry.get()),
+                () -> assertEquals("firstName1", optionalEmployeeDatabaseEntry.get().getFirstName()),
+                () -> assertEquals("lastName1", optionalEmployeeDatabaseEntry.get().getLastName())
+        );
+    }
+}
