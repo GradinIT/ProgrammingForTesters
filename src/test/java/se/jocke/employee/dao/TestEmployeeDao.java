e45851a1574b68621f14e5a5513e63c9451ce199
Gustav Gisselfeldt <gustav.gisselfeldt@gmail.com>
2021-03-05 12:29:31 +0100

    changed testemployeedao

--- src/test/java/se/jocke/employee/dao/TestEmployeeDao.java
+++ src/test/java/se/jocke/employee/dao/TestEmployeeDao.java
@@ -2,10 +2,8 @@ package se.jocke.employee.dao;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import se.jocke.H2JpaConfig;
 import se.jocke.LiquibaseConfigurer;
 import se.jocke.dao.EmployeeDao;
@@ -14,15 +12,14 @@ import se.jocke.dao.EmployeeDatabaseEntry;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {LiquibaseConfigurer.class, H2JpaConfig.class})
 public class TestEmployeeDao {
     @Autowired
     EmployeeDao employeeDao;
+
 
     @Test
     public void testGetEmployeeById() {
@@ -39,7 +36,7 @@ public class TestEmployeeDao {
         List<EmployeeDatabaseEntry> employees = employeeDao.findAll();
         Assertions.assertAll(
                 () -> assertNotNull(employees),
-                () ->assertEquals(employeeDao.findAll().size(),employees.size())
+                () ->assertEquals(3,employees.size())
         );
     }
 }
