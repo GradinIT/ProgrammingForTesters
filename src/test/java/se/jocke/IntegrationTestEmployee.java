999b818fd2bd0dba0ea763079e1a5225530de77f
Dogginator <douglas.kvarnstrom91@gmail.com>
2021-03-08 10:48:54 +0100

    Problems with Running IntegrationTest with more Paths...

---
+++ src/test/java/se/jocke/IntegrationTestEmployee.java
@@ -0,0 +1,9 @@
+package se.jocke;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/employee/employeeTest.feature")
+public class IntegrationTestEmployee {
+}
