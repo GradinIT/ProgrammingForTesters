782d6c43e89b8d36b785c3473385cb762021e5d7
jocke <joakim.gradin@gradin-it.com>
2021-04-19 00:16:38 +0200

    web

---
+++ src/main/java/se/jocke/config/Static_ResourceHandler.java
@@ -0,0 +1,23 @@
+package se.jocke.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class Static_ResourceHandler implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/public/**")
+                .addResourceLocations("classpath:/public/")
+                .setCachePeriod(3600)
+                .resourceChain(true) // 4.1
+                .addResolver(new PathResourceResolver()); //4.1
+        registry.addResourceHandler("/**") // « /webapp/css/style.css
+                .addResourceLocations("/");
+    }
+}
