ec069a4eb78309106ebf2da34311db9040f33945
jocke <joakim.gradin@gradin-it.com>
2021-10-13 14:50:21 +0200

    added example code

--- src/main/java/excersise/one/MyFirstClass.java
+++ src/main/java/excersise/one/MyFirstClass.java
@@ -2,4 +2,10 @@ package excersise.one;
 
 public class MyFirstClass {
     //TODO: create a method that makes this class runnable
+    public static void main(String[] args) {
+        for(int i = 0 ; i < args.length ; i++) {
+            System.out.println("index:" + i + " value in array:" +args[i]);
+        }
+        System.out.println("index:" + 4 + " value in array:" +args[4]);
+    }
 }
