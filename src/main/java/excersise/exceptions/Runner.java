7c38ec50c230d74e63004a873fb2a7a4ea158ee6
jocke <joakim.gradin@gradin-it.com>
2021-10-21 08:50:01 +0200

    ny övning

---
+++ src/main/java/excersise/exceptions/Runner.java
@@ -0,0 +1,13 @@
+package excersise.exceptions;
+
+public class Runner {
+
+    //Create a new class that extends RuntimeException
+    //in this class create a new method that throws an exception of the type you created
+    //in main create a new instance of the runner class ( this class )
+    // call the method on teh runner instance
+    //catch the exception and print the message to console
+    public static void main(String[] args) {
+
+    }
+}
