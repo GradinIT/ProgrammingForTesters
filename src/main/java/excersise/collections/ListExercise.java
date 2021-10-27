13ad3c102eaa33edb51da31096e250812299c5d8
jocke <joakim.gradin@gradin-it.com>
2021-10-27 09:46:14 +0200

    update code

---
+++ src/main/java/excersise/collections/ListExercise.java
@@ -0,0 +1,12 @@
+package excersise.collections;
+
+public class ListExercise {
+    public static void main(String[] args) {
+        //TODO: create a list of integers
+        // add 5 different integers to the list
+        // print all integers to console
+        // print size of list
+        // remove one integer from list
+        // print size of list
+    }
+}
