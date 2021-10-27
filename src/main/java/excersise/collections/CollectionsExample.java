9b56e573bed5a870fe19dee6d9809a47a8098d4e
jocke <joakim.gradin@gradin-it.com>
2021-10-27 12:26:59 +0200

    update code

--- src/main/java/excersise/collections/CollectionsExample.java
+++ src/main/java/excersise/collections/CollectionsExample.java
@@ -1,13 +1,16 @@
 package excersise.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class CollectionsExample {
     public static void main(String[] args) {
+      /*
         List<String> myList = new ArrayList<>();
         myList.add("Runar");
         myList.add("Runar");
@@ -32,5 +35,13 @@ public class CollectionsExample {
         integerSet.add(0);
         integerSet.add(17);
         integerSet.forEach(System.out::println);
+*/
+
+        Map<String,Integer> wordCounter = new HashMap<>();
+        wordCounter.put("the",1234);
+        wordCounter.put("They",888);
+
+        System.out.println(wordCounter.containsKey("the"));
+
     }
 }
--- src/main/java/excersise/collections/MapExercise.java
+++ src/main/java/excersise/collections/MapExercise.java
@@ -3,6 +3,12 @@ package excersise.collections;
 public class MapExercise {
     public static void main(String[] args) {
         //TODO: create a map with String as key and integer as value
+        //add 5 key value pairs to the map
+        // get the keys
+        // print keys to console
+        // get values
+        // print values to console
+        // change value fo one entry
 
     }
 }
