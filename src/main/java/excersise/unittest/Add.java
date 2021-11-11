6d34e7d083b97ce8f774b8176971cc9b08fea358
jocke <joakim.gradin@gradin-it.com>
2021-11-11 13:43:47 +0100

    junit test

---
+++ src/main/java/excersise/unittest/Add.java
@@ -0,0 +1,8 @@
+package excersise.unittest;
+
+// Övning 1
+public class Add {
+    public static int calculate ( int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/excersise/unittest/Divide.java
@@ -0,0 +1,9 @@
+package excersise.unittest;
+// Övning 2
+public class Divide {
+    public static double calculate( double a , double b) {
+        if(b <= 0)
+            throw new ArithmeticException("division by zero");
+        return a / b ;
+    }
+}
