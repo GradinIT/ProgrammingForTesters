4fea762a47cb1cfe2693d356692342c0d148c650
jocke <joakim.gradin@gradin-it.com>
2021-11-11 17:37:55 +0100

    junit test

--- src/test/java/se/jocke/CalculationTestSuite.java
+++ src/test/java/se/jocke/CalculationTestSuite.java
@@ -3,8 +3,10 @@ package se.jocke;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
-
+// Övning 5
 @RunWith(JUnitPlatform.class)
 @SelectPackages("se.jocke.excersices.basic")
 public class CalculationTestSuite {
+
 }
+
