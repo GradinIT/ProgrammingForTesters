b637e7141d8aac08e2ef2894bb95f14058165bb5
Erwin <erwin.marklund@gmail.com>
2021-11-24 14:01:05 +0100

    X

--- src/test/java/se/jocke/exercises/basic/TestAdd.java
+++ src/test/java/se/jocke/exercises/basic/TestAdd.java
@@ -11,6 +11,10 @@ public class TestAdd {
     public void testAdd1() {
         Assertions.assertEquals(12, Add.calculate(10,2));
     }
+
+    public void thing(){
+
+    }
     @Test
     public void testAdd2() {
         Assertions.assertEquals(0, Add.calculate(-5,5));
