17114b19cf12165bf560a073d6a21bfa7f39c88f
jocke <joakim.gradin@gradin-it.com>
2021-11-25 12:52:17 +0100

    unit-test tutorial

---
+++ src/test/java/se/jocke/department/api/model/TestDepartmentModel.java
@@ -0,0 +1,39 @@
+package se.jocke.department.api.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import se.jocke.department.api.DepartmentModel;
+import se.jocke.department.entity.Department;
+import se.jocke.department.test.builder.DepartmentModelTestBuilder;
+
+public class TestDepartmentModel {
+    private static final DepartmentModel DEPARTMENT_MODEL = DepartmentModelTestBuilder.build();
+    @Test
+    public void testThatDepartmentIsCreated () {
+        DepartmentModel departmentModel = DepartmentModel.builder()
+                .departmentId(DEPARTMENT_MODEL.getDepartmentId())
+                .departmentName(DEPARTMENT_MODEL.getDepartmentName())
+                .build();
+
+        Assertions.assertEquals(DEPARTMENT_MODEL,departmentModel);
+        Assertions.assertEquals(DEPARTMENT_MODEL.getDepartmentName(), departmentModel.getDepartmentName());
+        Assertions.assertEquals(DEPARTMENT_MODEL.getDepartmentId(), departmentModel.getDepartmentId());
+        String DEPARTMENT_MODEL_TO_STRING_VALUE = DEPARTMENT_MODEL.toString();
+        String department_model_to_string_value = departmentModel.toString();
+        Assertions.assertEquals(DEPARTMENT_MODEL_TO_STRING_VALUE,department_model_to_string_value);
+
+
+    }
+    @Test
+    public void testThatNullPointerExceptionIsRaisedWhenNotProvidingDepartmentId() {
+        Assertions.assertThrows(NullPointerException.class ,
+                () -> DepartmentModel.builder().departmentName(DEPARTMENT_MODEL.getDepartmentName()).build());
+
+    }
+    @Test
+    public void testThatNullPointerExceptionIsRaisedWhenNotProvidingDepartmentName() {
+        Assertions.assertThrows(NullPointerException.class ,
+                () -> DepartmentModel.builder().departmentId(DEPARTMENT_MODEL.getDepartmentId()).build());
+
+    }
+}
