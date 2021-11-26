bf98a956f931dd3447ba069a145c54d733241f11
Magnus Tällgård <Magnus.taellgaard@hotmail.com>
2021-11-26 16:45:41 +0100

    fixed some broken code in department

--- src/test/java/se/jocke/department/api/mapper/TestDepartmentModelMapper.java
+++ src/test/java/se/jocke/department/api/mapper/TestDepartmentModelMapper.java
@@ -2,16 +2,17 @@ package se.jocke.department.api.mapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import se.jocke.department.api.DepartmentModel;
-import se.jocke.department.builder.DepartmentModelTestBuilder;
-import se.jocke.department.builder.DepartmentTestBuilder;
-import se.jocke.department.entity.Department;
+import se.jocke.department.test.builder.DepartmentModelTestBuilder;
+import se.jocke.department.test.builder.DepartmentTestBuilder;
+import se.jocke.department.unittest.api.DepartmentModel;
+import se.jocke.department.unittest.api.mapper.DepartmentModelMapper;
+import se.jocke.department.unittest.entity.Department;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDepartmentModelMapper {
-    private final DepartmentModel DEPARTMENT_MODEL = DepartmentModelTestBuilder.builder().build();
-    private final Department DEPARTMENT = DepartmentTestBuilder.builder().build();
+    private final DepartmentModel DEPARTMENT_MODEL = DepartmentModelTestBuilder.build();
+    private final Department DEPARTMENT = DepartmentTestBuilder.build();
 
     @Test
     public void testDepartmentToDepartmentModelMapping() {
