bd321eb31295bc2418f0777b65b78331bf945de6
Magnus Tällgård <Magnus.taellgaard@hotmail.com>
2021-11-26 10:22:44 +0100

    Entity tests

--- src/test/java/se/jocke/employee/Builder/EmployeeTestBuilder.java
+++ src/test/java/se/jocke/employee/Builder/EmployeeTestBuilder.java
@@ -4,14 +4,16 @@ import se.jocke.department.entity.Department;
 import se.jocke.employee.entity.Employee;
 import se.jocke.employee.entity.EmployeeID;
 
+import java.math.BigDecimal;
+
 public class EmployeeTestBuilder {
     public static Employee.EmployeeBuilder builder() {
         return Employee.builder().employeeId(EmployeeID.builder().id(2).build())
                 .firstName("Joakim")
                 .lastName("Gradin")
-                .salary()
-                .fullTime()
-                .departmentId();
+                .salary(new BigDecimal("22000"))
+                .fullTime(Boolean.TRUE)
+                .departmentId(2);
 
     }
 }
