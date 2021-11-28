3ca9a27fd3ee7ce20999ad3bc3ea182b86decb2f
Fagelbert <albert.thornandersen@gmail.com>
2021-11-28 22:27:19 +0100

    EmployeeID problems

--- src/test/java/se/jocke/employee/entity/EmployeeTestFixture.java
+++ src/test/java/se/jocke/employee/entity/EmployeeTestFixture.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public class EmployeeTestFixture {
     public static final String name = "HEJ";
-    public static final EmployeeID id = EmployeeID.builder().id(1).build();
+    public static final EmployeeID id = EmployeeID.builder().id(3).build(); // ändra till EmployeeID istället för Integer
     public static String firstName = "Daniel";
     public static String lastName = "Hock";
     public static BigDecimal salary = new BigDecimal("10000");
