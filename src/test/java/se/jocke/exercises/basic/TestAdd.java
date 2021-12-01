1f62f935825b335ec2cebbe0fbd6e02722bd7b27
MikaelKarlsson91 <micke91k@gmail.com>
2021-12-01 14:45:47 +0100

    a

--- src/test/java/se/jocke/exercises/basic/TestAdd.java
+++ src/test/java/se/jocke/exercises/basic/TestAdd.java
@@ -9,18 +9,21 @@ import java.math.BigDecimal;
 public class TestAdd {
     @Test
     public void testAdd1() {
-        Assertions.assertEquals(12, Add.calculate(10,2));
-
-    public void thing(){
-        System.out.println("hej hå");
+        Add add = new Add();
+        Assertions.assertEquals(12, add.calculate(10,2));
     }
-
+    public void thing(){
+        System.out.println("hej");
+    }
     @Test
     public void testAdd2() {
-        Assertions.assertEquals(0, Add.calculate(-5,5));
+        Add add = new Add();
+        Assertions.assertEquals(0, add.calculate(-5,5));
     }
     @Test
-    public void testAdd3() {
-        Assertions.assertEquals(-2, Add.calculate(6,-8));
+    public void testAdd3()
+    {
+        Add add = new Add();
+        Assertions.assertEquals(-2, add.calculate(6,-8));
     }
 }
