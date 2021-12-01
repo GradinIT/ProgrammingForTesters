ae014754822ff92f5392dc7daf5d87a2b817fe51
jocke <joakim.gradin@gradin-it.com>
2021-12-01 13:28:51 +0100

    added DepartmentId and refactoring of code

--- src/main/java/se/jocke/department/api/mapper/DepartmentModelMapper.java
+++ src/main/java/se/jocke/department/api/mapper/DepartmentModelMapper.java
@@ -2,17 +2,18 @@ package se.jocke.department.api.mapper;
 
 import se.jocke.department.api.DepartmentModel;
 import se.jocke.department.entity.Department;
+import se.jocke.department.entity.DepartmentId;
 
 public class DepartmentModelMapper {
     public static Department map (DepartmentModel model) {
         return Department.builder()
                 .departmentName(model.getDepartmentName())
-                .departmentId(model.getDepartmentId())
+                .departmentId(DepartmentId.builder().id(model.getDepartmentId()).build())
                 .build();
     }
     public static DepartmentModel  map (Department department) {
         return DepartmentModel.builder()
-                .departmentId(department.getDepartmentId())
+                .departmentId(department.getDepartmentId().getId())
                 .departmentName(department.getDepartmentName())
                 .build();
     }
--- src/main/java/se/jocke/department/entity/DepartmentId.java
+++ src/main/java/se/jocke/department/entity/DepartmentId.java
@@ -1,2 +1,9 @@
-package se.jocke.department.entity;public class DepartmentId {
+package se.jocke.department.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+import se.jocke.common.entity.EntityID;
+@SuperBuilder
+@EqualsAndHashCode
+public class DepartmentId extends EntityID<Integer> {
 }
