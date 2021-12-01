db5cefc4826a2764e8c981221a6ab1185e497f20
albertthorn <albert.thornandersen@gmail.com>
2021-12-01 17:28:10 +0100

    Revert "TestEmployeeModelMapper: la till testEmployeeToEmployeeModelMapping - verkar funka men mvn clean install kör färre test?"
    This reverts commit c5c3a26c077b6c41384255c4318f0316bdfae38d.

--- src/test/java/se/jocke/employee/api/mapper/TestEmployeeModelMapper.java
+++ src/test/java/se/jocke/employee/api/mapper/TestEmployeeModelMapper.java
@@ -7,9 +7,6 @@ import se.jocke.employee.builder.EmployeeModelTestBuilder;
 import se.jocke.employee.builder.EmployeeTestBuilder;
 import se.jocke.employee.entity.Employee;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 public class TestEmployeeModelMapper {
     private static final Employee EMPLOYEE = EmployeeTestBuilder.build();
     private static final EmployeeModel EMPLOYEE_MODEL = EmployeeModelTestBuilder.build();
@@ -23,40 +20,26 @@ public class TestEmployeeModelMapper {
             .build();
             */
 
-
     @Test
     public void testThatEmployeeModelIsEqualToEmployee() {
         EmployeeModel employeeModel = EmployeeModelMapper.map(EMPLOYEE);
-        assertEquals(EMPLOYEE_MODEL, employeeModel);
-        assertEquals(EMPLOYEE.getFirstName(), employeeModel.getFirstName());
-        assertEquals(EMPLOYEE.getDepartmentId(), employeeModel.getDepartmentId());
-        assertEquals(EMPLOYEE.getLastName(), employeeModel.getLastName());
-        assertEquals(EMPLOYEE.getSalary(), employeeModel.getSalary());
-        assertEquals(EMPLOYEE.getEmployeeId(), employeeModel.getEmployeeId());
+        Assertions.assertEquals(EMPLOYEE_MODEL,employeeModel);
+        Assertions.assertEquals(EMPLOYEE.getFirstName(),employeeModel.getFirstName());
+        Assertions.assertEquals(EMPLOYEE.getDepartmentId(),employeeModel.getDepartmentId());
+        Assertions.assertEquals(EMPLOYEE.getLastName(), employeeModel.getLastName());
+        Assertions.assertEquals(EMPLOYEE.getSalary(), employeeModel.getSalary());
+        Assertions.assertEquals(EMPLOYEE.getEmployeeId(), employeeModel.getEmployeeId());
     }
 
     @Test
     public void testThatEmployeeIsEqualToEmployeeModel() {
         Employee employee = EmployeeModelMapper.map(EMPLOYEE_MODEL);
-        assertEquals(EMPLOYEE, employee);
-        assertEquals(EMPLOYEE_MODEL.getDepartmentId(), employee.getDepartmentId());
-        assertEquals(EMPLOYEE_MODEL.getFirstName(), employee.getFirstName());
-        assertEquals(EMPLOYEE_MODEL.getLastName(), employee.getLastName());
-        assertEquals(EMPLOYEE_MODEL.getEmployeeId(), employee.getEmployeeId());
-        assertEquals(EMPLOYEE_MODEL.getFullTime(), employee.getFullTime());
-        assertEquals(EMPLOYEE_MODEL.getSalary(), employee.getSalary());
-    }
-
-    @Test
-    public void testEmployeeToEmployeeModelMapping() {
-        EmployeeModel model = EmployeeModelMapper.map(EMPLOYEE);
-        Assertions.assertAll(
-                () -> assertEquals(EMPLOYEE.getEmployeeId(), model.getEmployeeId()), //.getId() här??
-                () -> assertEquals(EMPLOYEE.getFirstName(), model.getFirstName()),
-                () -> assertEquals(EMPLOYEE.getLastName(), model.getLastName()),
-                () -> assertEquals(EMPLOYEE.getSalary(), model.getSalary()),
-                () -> assertEquals(EMPLOYEE.getDepartmentId(), model.getDepartmentId()),
-                () -> assertEquals(EMPLOYEE.getFullTime(), model.getFullTime())
-        );
+        Assertions.assertEquals(EMPLOYEE, employee);
+        Assertions.assertEquals(EMPLOYEE_MODEL.getDepartmentId(),employee.getDepartmentId());
+        Assertions.assertEquals(EMPLOYEE_MODEL.getFirstName(),employee.getFirstName());
+        Assertions.assertEquals(EMPLOYEE_MODEL.getLastName(),employee.getLastName());
+        Assertions.assertEquals(EMPLOYEE_MODEL.getEmployeeId(),employee.getEmployeeId());
+        Assertions.assertEquals(EMPLOYEE_MODEL.getFullTime(),employee.getFullTime());
+        Assertions.assertEquals(EMPLOYEE_MODEL.getSalary(), employee.getSalary());
     }
 }
