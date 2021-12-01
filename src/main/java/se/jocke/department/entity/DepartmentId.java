39ee287231ccd14b24ed6732249daaf45e184c7c
jocke <joakim.gradin@gradin-it.com>
2021-12-01 13:25:27 +0100

    added DepartmentId and refactoring of code

--- src/main/java/se/jocke/department/entity/DepartmentId.java
+++ src/main/java/se/jocke/department/entity/DepartmentId.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package se.jocke.department.entity;
 
 import lombok.EqualsAndHashCode;
@@ -7,4 +8,7 @@ import se.jocke.common.entity.EntityID;
 @SuperBuilder
 @EqualsAndHashCode
 public class DepartmentId extends EntityID<Integer> {
+=======
+package se.jocke.department.entity;public class DepartmentId {
+>>>>>>> fb0ac34 ( added DepartmentId and refactoring of code)
 }
