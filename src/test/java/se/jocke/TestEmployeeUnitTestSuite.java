8d03dab04b7f780019a65836ec4cfe7322700b2a
albertthorn <albert.thornandersen@gmail.com>
2021-12-03 11:39:30 +0100

    TestEmployeeService added

--- src/test/java/se/jocke/TestEmployeeUnitTestSuite.java
+++ src/test/java/se/jocke/TestEmployeeUnitTestSuite.java
@@ -1,11 +1,14 @@
+/*
 package se.jocke;
+
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
 @SelectPackages({
-        "se.jocke.department.unittest"
+        "se.jocke.employee.unittest"
 })
 public class TestEmployeeUnitTestSuite {
 }
+*/
