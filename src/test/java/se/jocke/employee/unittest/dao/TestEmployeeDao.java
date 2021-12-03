ef929e6bc223576ef4d388a79c532998f33a776f
albertthorn <albert.thornandersen@gmail.com>
2021-12-03 13:58:35 +0100

    fixed TestE.U.TestSuite o mer på Dao

--- src/test/java/se/jocke/employee/unittest/dao/TestEmployeeDao.java
+++ src/test/java/se/jocke/employee/unittest/dao/TestEmployeeDao.java
@@ -2,6 +2,12 @@ package se.jocke.employee.unittest.dao;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import se.jocke.config.H2JpaConfig;
+import se.jocke.config.LiquibaseConfigurer;
 import se.jocke.employee.dao.EmployeeDao;
 import se.jocke.employee.dao.EmployeeDatabaseEntry;
 
@@ -11,15 +17,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {LiquibaseConfigurer.class, H2JpaConfig.class})
 public class TestEmployeeDao {
 
     // gå igenom och förstå, lägg till mer?
-
+    @Autowired
     EmployeeDao employeeDao;
 
     @Test
     public void testGetEmployeeById() {
-        Integer departmetId = 1; // ?
+        Integer departmetId = 2; // ?
         Optional<EmployeeDatabaseEntry> optionalEmployeeDatabaseEntry = employeeDao.findById(departmetId);
      /*   Assertions.assertAll(
                 () -> assertTrue(optionalEmployeeDatabaseEntry.isPresent()),
