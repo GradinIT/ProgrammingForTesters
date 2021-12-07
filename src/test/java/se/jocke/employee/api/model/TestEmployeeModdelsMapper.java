6a3f4dc940729e45374021c8e962fb5ffc502582
bjornbonnevier <bonnevier.bjorn@gmail.com>
2021-12-07 12:36:04 +0100

    ny api mapp med tester

---
+++ src/test/java/se/jocke/employee/api/model/TestEmployeeModdelsMapper.java
@@ -0,0 +1,31 @@
+package se.jocke.department.unittest.api.model;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import se.jocke.department.api.DepartmentModel;
+import se.jocke.department.api.mapper.DepartmentModelsMapper;
+import se.jocke.department.entity.Department;
+import se.jocke.department.test.builder.DepartmentModelTestBuilder;
+import se.jocke.department.test.builder.DepartmentTestBuilder;
+import se.jocke.employee.api.EmployeeModel;
+import se.jocke.employee.api.mapper.EmployeeModelsMapper;
+import se.jocke.employee.builder.EmployeeModelTestBuilder;
+import se.jocke.employee.builder.EmployeeTestBuilder;
+import se.jocke.employee.entity.Employee;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestEmployeeModdelsMapper {
+    @Test
+    public void testEmployeeModelsMapper() {
+        List<Employee> EMPLOYEES = Arrays.asList(EmployeeTestBuilder.builder().build());
+        List<EmployeeModel> EMPLOYEE_MODELS= Arrays.asList(EmployeeModelTestBuilder.builder().build());
+        List<EmployeeModel> employeeModels = EmployeeModelsMapper.map(EMPLOYEES);
+        Assertions.assertAll(
+                ()->Assertions.assertNotNull(employeeModels),
+                ()->Assertions.assertEquals(EMPLOYEE_MODELS,employeeModels)
+        );
+    }
+}
