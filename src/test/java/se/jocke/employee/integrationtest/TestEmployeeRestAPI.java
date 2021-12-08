395e56c4f84b8c658b23859aca6f22b209e1f894
jocke <joakim.gradin@gradin-it.com>
2021-12-08 14:23:51 +0100

    added test cases

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -8,25 +8,30 @@ public class TestEmployeeRestAPI extends EmployeeTestClient {
 
     @When("the client calls \\/employee")
     public void the_client_calls_employee() {
-     }
-   @Then("the client receives {int} employees")
+    }
+
+    @Then("the client receives {int} employees")
     public void the_client_receives_employees(Integer int1) {
     }
 
     @Given("the employees")
     public void the_employees(io.cucumber.datatable.DataTable dataTable) {
     }
-   @When("the client gets employee {int}")
+
+    @When("the client gets employee {int}")
     public void the_client_gets_employee(Integer int1) {
-     }
-   @Then("firstname is (.+)")
+    }
+
+    @Then("firstname is (.+)")
     public void firstname_is_runar() {
-     }
-   @When("the client updates firstname for employee to (.+)")
+    }
+
+    @When("the client updates firstname for employee to (.+)")
     public void the_client_updates_firstname_for_employee_to_runar() {
     }
+
     @Then("the firstname is updated to (.+)")
     public void the_firstname_is_updated_to_runar() {
-     }
+    }
 
 }
