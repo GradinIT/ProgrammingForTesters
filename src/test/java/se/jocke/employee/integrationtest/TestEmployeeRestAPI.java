567f76f35e5b4a1cc487a34099d16991eeae3fb4
jocke <joakim.gradin@gradin-it.com>
2021-12-08 17:12:52 +0100

    test cases implemented

--- src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
+++ src/test/java/se/jocke/employee/integrationtest/TestEmployeeRestAPI.java
@@ -4,6 +4,7 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 import se.jocke.employee.api.EmployeeModel;
 
 import java.math.BigDecimal;
@@ -11,33 +12,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestEmployeeRestAPI extends EmployeeTestClient {
-
+    private List<EmployeeModel> employees;
+    private EmployeeModel employeeModel;
     @When("the client calls \\/employee")
     public void the_client_calls_employee() {
+        employees = getAllEmployees().get();
     }
 
     @Then("the client receives {int} employees")
-    public void the_client_receives_employees(Integer int1) {
+    public void the_client_receives_employees(Integer numberOfEmployees) {
+        Assertions.assertEquals(numberOfEmployees,employees.size());
     }
-
 
     @When("the client gets employee {int}")
-    public void the_client_gets_employee(Integer int1) {
+    public void the_client_gets_employee(Integer employeeId) {
+        employeeModel = getEmployeeById(employeeId).get();
     }
 
-    @Then("firstname is (.+)")
-    public void firstname_is_runar() {
+    @Then("^firstname is (.+)$")
+    public void firstname_is(String firstname) {
+        Assertions.assertEquals(firstname,employeeModel.getFirstName());
     }
 
-    @When("the client updates firstname for employee to (.+)")
-    public void the_client_updates_firstname_for_employee_to_runar() {
+    @When("^the client updates firstname for employee whit id (\\d+) to (.+)$")
+    public void the_client_updates_firstname_for_employee_to_runar(Integer emplyeeId, String firstName ) {
+        employeeModel = getEmployeeById(emplyeeId).get();
+        EmployeeModel update = EmployeeModel.builder()
+                .departmentId(employeeModel.getDepartmentId())
+                .employeeId(employeeModel.getEmployeeId())
+                .fullTime(employeeModel.getFullTime())
+                .salary(employeeModel.getSalary())
+                .lastName(employeeModel.getLastName())
+                .firstName(firstName)
+                .build();
+        employeeModel = updateEmployee(update).get();
     }
 
-    @Then("the firstname is updated to (.+)")
-    public void the_firstname_is_updated_to_runar() {
+    @Then("^the firstname is updated to (.+)$")
+    public void the_firstname_is_updated_to_runar(String firstName) {
+        Assertions.assertEquals(firstName,employeeModel.getFirstName());
     }
 
-    @Given("the employees")
+    @Given("test data")
     public void the_employees(DataTable dataTable) {
         makeDepartmentList(dataTable.asList())
                 .stream()
