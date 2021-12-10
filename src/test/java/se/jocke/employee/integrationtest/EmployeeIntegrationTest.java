e20842682fcdc2a16a3d0951b53e29e4d42dbc5f
HgergoH <gergosimonyi@hotmail.com>
2021-12-10 11:38:41 +0100

    last change

--- src/test/java/se/jocke/employee/integrationtest/EmployeeIntegrationTest.java
+++ src/test/java/se/jocke/employee/integrationtest/EmployeeIntegrationTest.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = "classpath:features/employee/employeeTest.feature")
-//public class EmployeeIntegrationTest {
-//}
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features/employee/employeeTest.feature")
+public class EmployeeIntegrationTest {
+}
