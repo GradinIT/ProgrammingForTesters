3586fab9f79178f8e6edbd40798d8018b21e514a
albertthorn <albert.thornandersen@gmail.com>
2021-12-15 13:54:18 +0100

    la till E.Service tester

--- src/test/java/se/jocke/employee/test/builder/EmployeeDatabaseEntryTestBuilder.java
+++ src/test/java/se/jocke/employee/test/builder/EmployeeDatabaseEntryTestBuilder.java
@@ -1,6 +1,8 @@
 package se.jocke.employee.test.builder;
-
 import se.jocke.employee.dao.EmployeeDatabaseEntry;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EmployeeDatabaseEntryTestBuilder {
 
@@ -15,4 +17,14 @@ public class EmployeeDatabaseEntryTestBuilder {
                     .build();
 
         }
+    public static List<EmployeeDatabaseEntry> buildList() {
+        return Arrays.asList(EmployeeDatabaseEntry.builder()
+                .employeeId(EmployeeTestFixture.id.getId())
+                .firstName(EmployeeTestFixture.firstName)
+                .lastName(EmployeeTestFixture.lastName)
+                .salary(EmployeeTestFixture.salary)
+                .fullTime(EmployeeTestFixture.fullTime)
+                .departmentId(EmployeeTestFixture.departmentId)
+                .build());
+    }
     }
