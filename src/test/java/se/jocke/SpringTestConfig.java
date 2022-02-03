a886d54bb9dc4fbd0a2d88e7db2d3392b295c055
Joakim Gradin <joakimgradin@neo.local>
2022-02-03 23:15:27 +0100

    fix

--- src/test/java/se/jocke/SpringTestConfig.java
+++ src/test/java/se/jocke/SpringTestConfig.java
@@ -1,6 +1,5 @@
 package se.jocke;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -9,7 +8,6 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @TestConfiguration
-@EnableAutoConfiguration
 public class SpringTestConfig {
     @Bean
     @Primary
