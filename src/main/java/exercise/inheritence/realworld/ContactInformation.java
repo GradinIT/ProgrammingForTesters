e806435be8090d408659f58f8422f0ff46deb18a
Joakim Gradin <joakimgradin@neo.local>
2022-02-04 01:57:53 +0100

    removing crap

--- src/main/java/exercise/inheritence/realworld/ContactInformation.java
+++
@@ -1,8 +0,0 @@
-package exercise.inheritence.realworld;
-
-import lombok.experimental.SuperBuilder;
-
-@SuperBuilder
-public abstract class ContactInformation implements Validatable{
-    private final PersonId id;
-}
