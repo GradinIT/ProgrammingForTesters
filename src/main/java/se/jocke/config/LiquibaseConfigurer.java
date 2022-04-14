7a9fc0680a2d556da10e0d80943284eca30ce77f
Joakim Gradin <joakimgradin@neo.local>
2022-04-14 12:06:24 +0200

    fix

--- src/main/java/se/jocke/config/LiquibaseConfigurer.java
+++ src/main/java/se/jocke/config/LiquibaseConfigurer.java
@@ -6,20 +6,27 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 
 @Configuration
 public class LiquibaseConfigurer {
-
-    @Autowired
+    @Bean
     @Qualifier("primaryDataSource")
-    private DataSource dataSource;
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+        return dataSource;
+    }
 
     @Bean
     @DependsOn
-    public SpringLiquibase liquibase() {
+    public SpringLiquibase liquibase(DataSource dataSource) {
         SpringLiquibase liquibase = new SpringLiquibase();
         liquibase.setChangeLog("classpath:liquibase/liquibase-changelog.xml");
         liquibase.setDataSource(dataSource);
